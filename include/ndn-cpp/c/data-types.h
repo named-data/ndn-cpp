fa05233b3c5dd164ce30da6d873bf8f2bcd8582d
Jeff Thompson <jefft0@gmail.com>
2014-01-22 19:09:59 -0800

    MetaInfo: In ContentType, mark ENCR, GONE and NACK as not supported.

--- include/ndn-cpp/c/data-types.h
+++ include/ndn-cpp/c/data-types.h
@@ -11,12 +11,16 @@
 extern "C" {
 #endif
 
+/** ndn_ContentType defines constants for the MetaInfo "type" field.  Note that the constants for BLOB, LINK and KEY
+ * are the same as defined in the NDN-TLV spec.
+ */
 typedef enum {
   ndn_ContentType_BLOB = 0,
-  // ndn_ContentType_DATA is deprecated.  Use ndn_ContentType_BLOB.
+  // ContentType DATA is deprecated.  Use ndn_ContentType_BLOB.
   ndn_ContentType_DATA = 0,
   ndn_ContentType_LINK = 1,
   ndn_ContentType_KEY =  2,
+  // ContentType ENCR, GONE and NACK are not supported in NDN-TLV encoding and are deprecated.
   ndn_ContentType_ENCR = 3,
   ndn_ContentType_GONE = 4,
   ndn_ContentType_NACK = 5
