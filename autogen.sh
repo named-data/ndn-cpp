0ebf0344ea83a3262c43cd61977e9a0d272da17d
Jeff Thompson <jefft0@gmail.com>
2014-01-24 12:37:05 -0800

    Make: In autogen.sh, don't run ./configure since the user may want to run ./configure with options.

--- autogen.sh
+++ autogen.sh
@@ -3,4 +3,5 @@
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 autoreconf --force --install --verbose "$srcdir"
-test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+# Don't run ./configure since the user may want to run ./configure with options.
+echo "" ; echo "You need to run ./configure again."
