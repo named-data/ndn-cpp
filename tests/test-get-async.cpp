60f6d5e0d6c030a92b5e2812cf54ebf859df9c1b
Jeff Thompson <jefft0@gmail.com>
2014-02-12 14:49:03 -0800

    tests: Change from deprecated to_uri to toUri.

--- tests/test-get-async.cpp
+++ tests/test-get-async.cpp
@@ -28,7 +28,7 @@ public:
   void onData(const ptr_lib::shared_ptr<const Interest>& interest, const ptr_lib::shared_ptr<Data>& data)
   {
     ++callbackCount_;
-    cout << "Got data packet with name " << data->getName().to_uri() << endl;
+    cout << "Got data packet with name " << data->getName().toUri() << endl;
     for (size_t i = 0; i < data->getContent().size(); ++i)
       cout << (*data->getContent())[i];
     cout << endl;  
