99977d9aa9a170eee2a6aef79c002f3f476c8b41
Jeff Thompson <jefft0@gmail.com>
2014-04-22 17:35:33 -0700

    security: Added initial stub of FilePrivateKeyStorage.

---
+++ include/ndn-cpp/security/identity/file-private-key-storage.hpp
@@ -0,0 +1,110 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * @author: Yingdi Yu <yingdi@cs.ucla.edu>
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * See COPYING for copyright and distribution information.
+ */
+
+#ifndef NDN_FILE_PRIVATE_KEY_STORAGE_HPP
+#define NDN_FILE_PRIVATE_KEY_STORAGE_HPP
+
+#include <string>
+#include "private-key-storage.hpp"
+
+namespace ndn {
+
+/**
+ * FilePrivateKeyStorage extends PrivateKeyStorage to implement private key
+ * storage using files.
+ */
+class FilePrivateKeyStorage : public PrivateKeyStorage {
+public:
+  /**
+   * Create a new FilePrivateKeyStorage to connect to the default directory.
+   */
+  FilePrivateKeyStorage();
+  
+  /**
+   * The virtual Destructor.
+   */
+  virtual 
+  ~FilePrivateKeyStorage();
+
+  /**
+   * Generate a pair of asymmetric keys.
+   * @param keyName The name of the key pair.
+   * @param keyType The type of the key pair, e.g. KEY_TYPE_RSA.
+   * @param keySize The size of the key pair.
+   */
+  virtual void 
+  generateKeyPair(const Name& keyName, KeyType keyType = KEY_TYPE_RSA, int keySize = 2048);
+
+  /**
+   * Get the public key
+   * @param keyName The name of public key.
+   * @return The public key.
+   */
+  virtual ptr_lib::shared_ptr<PublicKey> 
+  getPublicKey(const Name& keyName);
+  
+  /**
+   * Fetch the private key for keyName and sign the data, returning a signature Blob.
+   * @param data Pointer to the input byte array.
+   * @param dataLength The length of data.
+   * @param keyName The name of the signing key.
+   * @param digestAlgorithm the digest algorithm.
+   * @return The signature, or a null pointer if signing fails.
+   */  
+  virtual Blob 
+  sign(const uint8_t *data, size_t dataLength, const Name& keyName, DigestAlgorithm digestAlgorithm = DIGEST_ALGORITHM_SHA256);
+  
+  /**
+   * Decrypt data.
+   * @param keyName The name of the decrypting key.
+   * @param data The byte to be decrypted.
+   * @param dataLength the length of data.
+   * @param isSymmetric If true symmetric encryption is used, otherwise asymmetric encryption is used.
+   * @return The decrypted data.
+   */
+  virtual Blob 
+  decrypt(const Name& keyName, const uint8_t* data, size_t dataLength, bool isSymmetric = false);
+
+  /**
+   * Encrypt data.
+   * @param keyName The name of the encrypting key.
+   * @param data The byte to be encrypted.
+   * @param dataLength the length of data.
+   * @param isSymmetric If true symmetric encryption is used, otherwise asymmetric encryption is used.
+   * @return The encrypted data.
+   */
+  virtual Blob
+  encrypt(const Name& keyName, const uint8_t* data, size_t dataLength, bool isSymmetric = false);
+
+  /**
+   * @brief Generate a symmetric key.
+   * @param keyName The name of the key.
+   * @param keyType The type of the key, e.g. KEY_TYPE_AES.
+   * @param keySize The size of the key.
+   */
+  virtual void 
+  generateKey(const Name& keyName, KeyType keyType = KEY_TYPE_AES, int keySize = 256);
+
+  /**
+   * Check if a particular key exists.
+   * @param keyName The name of the key.
+   * @param keyClass The class of the key, e.g. KEY_CLASS_PUBLIC, KEY_CLASS_PRIVATE, or KEY_CLASS_SYMMETRIC.
+   * @return True if the key exists, otherwise false.
+   */
+  virtual bool
+  doesKeyExist(const Name& keyName, KeyClass keyClass);  
+
+private:
+  std::string
+  nameTransform(const std::string& keyName, const std::string& extension);
+
+  std::string keystorePath_;
+};
+
+}
+
+#endif
---
+++ src/security/identity/file-private-key-storage.cpp
@@ -0,0 +1,187 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * @author: Yingdi Yu <yingdi@cs.ucla.edu>
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * See COPYING for copyright and distribution information.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <algorithm>
+#include <fstream>
+#include "../../c/util/crypto.h"
+#include <ndn-cpp/security/security-exception.hpp>
+#include <ndn-cpp/security/identity/file-private-key-storage.hpp>
+
+using namespace std;
+
+namespace ndn {
+
+static const char *WHITESPACE_CHARS = " \n\r\t";
+
+/**
+ * Modify str in place to erase whitespace on the left.
+ * @param str
+ */
+static inline void 
+trimLeft(string& str)
+{
+  size_t found = str.find_first_not_of(WHITESPACE_CHARS);
+  if (found != string::npos) {
+    if (found > 0)
+      str.erase(0, found);
+  }
+  else
+    // All whitespace
+    str.clear();    
+}
+
+/**
+ * Modify str in place to erase whitespace on the right.
+ * @param str
+ */
+static inline void 
+trimRight(string& str)
+{
+  size_t found = str.find_last_not_of(WHITESPACE_CHARS);
+  if (found != string::npos) {
+    if (found + 1 < str.size())
+      str.erase(found + 1);
+  }
+  else
+    // All whitespace
+    str.clear();
+}
+
+/**
+ * Modify str in place to erase whitespace on the left and right.
+ * @param str
+ */
+static void 
+trim(string& str)
+{
+  trimLeft(str);
+  trimRight(str);
+}
+
+FilePrivateKeyStorage::FilePrivateKeyStorage()
+{
+  // Note: We don't use <filesystem> support because it is not "header-only" and 
+  // require linking to libraries.
+  // TODO: Handle non-unix file system paths which don't use '/'.
+  const char* home = getenv("HOME");
+  if (!home || *home == '\0')
+    // Don't expect this to happen;
+    home = ".";
+  string homeDir(home);
+  if (homeDir[homeDir.size() - 1] == '/')
+    // Strip the ending '/'.
+    homeDir.erase(homeDir.size() - 1);
+  
+  keystorePath_ = homeDir + '/' + ".ndn/ndnsec-tpm-file";
+  // TODO: Handle non-unix file systems which don't have "mkdir -p".
+  ::system(("mkdir -p " + keystorePath_).c_str());
+}
+
+FilePrivateKeyStorage::~FilePrivateKeyStorage()
+{
+}
+
+void 
+FilePrivateKeyStorage::generateKeyPair
+  (const Name& keyName, KeyType keyType, int keySize)
+{
+#if 1
+  throw runtime_error("FilePrivateKeyStorage::generateKeyPair not implemented");
+#endif
+}
+
+ptr_lib::shared_ptr<PublicKey> 
+FilePrivateKeyStorage::getPublicKey(const Name& keyName)
+{
+  string keyURI = keyName.toUri();
+
+  if (!doesKeyExist(keyName, KEY_CLASS_PUBLIC))
+    throw SecurityException("Public Key does not exist.");
+
+  ifstream file(nameTransform(keyURI, ".pub").c_str());
+  stringstream base64;
+  base64 << file.rdbuf();
+
+#if 1
+  cout << "base64 " << base64 << endl;
+  throw runtime_error("FilePrivateKeyStorage::getPublicKey not implemented");
+#else
+  return PublicKey::fromDer(Blob(os.str().c_str(), os.str().size()));
+#endif
+}
+
+Blob 
+FilePrivateKeyStorage::sign
+  (const uint8_t *data, size_t dataLength, const Name& keyName, 
+   DigestAlgorithm digestAlgorithm)
+{
+#if 1
+  throw runtime_error("FilePrivateKeyStorage::sign not implemented");
+#endif
+}
+
+Blob 
+FilePrivateKeyStorage::decrypt
+  (const Name& keyName, const uint8_t* data, size_t dataLength, 
+   bool isSymmetric)
+{
+#if 1
+  throw runtime_error("FilePrivateKeyStorage::decrypt not implemented");
+#endif
+}
+
+Blob
+FilePrivateKeyStorage::encrypt
+  (const Name& keyName, const uint8_t* data, size_t dataLength, 
+   bool isSymmetric)
+{
+#if 1
+  throw runtime_error("FilePrivateKeyStorage::encrypt not implemented");
+#endif
+}
+
+void 
+FilePrivateKeyStorage::generateKey
+  (const Name& keyName, KeyType keyType, int keySize)
+{
+#if 1
+  throw runtime_error("FilePrivateKeyStorage::generateKey not implemented");
+#endif
+}
+
+bool
+FilePrivateKeyStorage::doesKeyExist(const Name& keyName, KeyClass keyClass)
+{
+  string keyURI = keyName.toUri();
+  if (keyClass == KEY_CLASS_PUBLIC)
+    return ::access(nameTransform(keyURI, ".pub").c_str(), R_OK) == 0;
+  else if (keyClass == KEY_CLASS_PRIVATE)
+    return ::access(nameTransform(keyURI, ".pri").c_str(), R_OK) == 0;
+  else if (keyClass == KEY_CLASS_SYMMETRIC)
+    return ::access(nameTransform(keyURI, ".key").c_str(), R_OK) == 0;
+  else
+    return false;
+}
+
+string
+FilePrivateKeyStorage::nameTransform
+  (const string& keyName, const string& extension)
+{
+  uint8_t hash[SHA256_DIGEST_LENGTH];
+  ndn_digestSha256((uint8_t*)&keyName[0], keyName.size(), hash);
+  
+  string digest = toBase64(hash, sizeof(hash));
+  trim(digest);
+  std::replace(digest.begin(), digest.end(), '/', '%');
+
+  return keystorePath_ + "/" + digest + extension;
+}
+
+}
