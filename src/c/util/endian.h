cc802c0bd9f8a8c8e6c93e633a039181fc6d380c
Jeff Thompson <jefft0@gmail.com>
2014-05-16 17:27:11 -0700

    util: In endian.h, added _WIN32.

--- src/c/util/endian.h
+++ src/c/util/endian.h
@@ -7,7 +7,10 @@
  * Author: Junxiao Shi <shijunxiao@email.arizona.edu>
  */
 
-#ifdef __APPLE__
+#ifndef NDN_ENDIAN_H
+#define NDN_ENDIAN_H
+
+#if defined(__APPLE__)
 
 #include <libkern/OSByteOrder.h>
 #define htobe16(x) OSSwapHostToBigInt16(x)
@@ -23,9 +26,43 @@
 #define be64toh(x) OSSwapBigToHostInt64(x)
 #define le64toh(x) OSSwapLittleToHostInt64(x)
 
-#else
+#elif defined(_WIN32)
 
-#ifdef __FreeBSD__
+#include <WinSock2.h>
+#include <stdint.h>
+#include <ndn-cpp/ndn-cpp-config.h>
+#define htobe16(x) htons(x)
+#define be16toh(x) ntohs(x)
+#define htobe32(x) htonl(x)
+#define be32toh(x) ntohl(x)
+#if NDN_CPP_HAVE_HTONLL
+// Windows 8 has htonll. Assume we have ntohll if we have htonll.
+#define htobe64(x) htonll(x)
+#define be64toh(x) ntohll(x)
+#else
+// Define directly.
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// htons(1) == 1 is true for big endian systems.
+static __inline uint64_t htobe64(uint64_t x) { return htons(1) == 1 ? x : (((uint64_t)htonl(x)) << 32) + htonl(x >> 32); }
+static __inline uint64_t be64toh(uint64_t x) { return htons(1) == 1 ? x : (((uint64_t)ntohl(x)) << 32) + ntohl(x >> 32); }
+
+#ifdef  __cplusplus
+}
+#endif
+
+#endif
+//#define htole16(x)
+//#define le16toh(x)
+//#define htole32(x)
+//#define le32toh(x)
+//#define htole64(x)
+//#define le64toh(x)
+
+#elif defined(__FreeBSD__)
 #include <sys/endian.h>
 #else
 // Linux, Cygwin, etc.
