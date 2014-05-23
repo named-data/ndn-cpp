085c2c9e58a3aa855601e622f7ed6d3cf7a58de7
Jeff Thompson <jefft0@gmail.com>
2014-05-22 17:34:13 -0700

    global: In the license, add an exception to allow OpenSSL.

--- src/util/dynamic-uint8-vector.cpp
+++ src/util/dynamic-uint8-vector.cpp
@@ -6,7 +6,8 @@
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
