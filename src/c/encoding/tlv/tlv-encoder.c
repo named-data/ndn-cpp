8ebf4c57d1e39a599466d72387da1b47a01cf33a
Jeff Thompson <jefft0@remap.ucla.edu>
2014-08-25 13:07:18 -0700

    encoding: Fix bug in TlvEncoder: In sizeOfNonNegativeInteger and writeNonNegativeIntegerEnabled, should encode integers <= 0xff as one byte. Refs #1924.

--- src/c/encoding/tlv/tlv-encoder.c
+++ src/c/encoding/tlv/tlv-encoder.c
@@ -94,7 +94,7 @@ ndn_TlvEncoder_writeNonNegativeIntegerEnabled(struct ndn_TlvEncoder *self, uint6
 {
   ndn_Error error;
 
-  if (value < 253) {
+  if (value <= 0xff) {
     if ((error = ndn_DynamicUInt8Array_ensureLength(self->output, self->offset + 1)))
       return error;
     self->output->array[self->offset] = (uint8_t)value;
