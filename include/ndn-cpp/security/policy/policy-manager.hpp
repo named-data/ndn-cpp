2860b0773f7e8b43cbe3674aca7ab879106530c4
Jeff Thompson <jefft0@remap.ucla.edu>
2014-09-12 12:23:34 -0700

    security (minor): Fix typo in doc for PolicyManager::checkVerificationPolicy.

--- include/ndn-cpp/security/policy/policy-manager.hpp
+++ include/ndn-cpp/security/policy/policy-manager.hpp
@@ -94,9 +94,9 @@ public:
    * @param interest The interest with the signature to check.
    * @param stepCount The number of verification steps that have been done, used
    * to track the verification progress.
-   * @param onVerified If the signature is verified, this calls onVerified(data).
+   * @param onVerified If the signature is verified, this calls onVerified(interest).
    * @param onVerifyFailed If the signature check fails, this calls
-   * onVerifyFailed(data).
+   * onVerifyFailed(interest).
    * @return the indication of next verification step, null if there is no
    * further step.
    */
