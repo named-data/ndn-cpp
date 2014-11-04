3fdb83854dbcb14669aaba62cec51d714c2956c7
Jeff Thompson <jefft0@remap.ucla.edu>
2014-11-04 09:58:42 -0800

    ndnboost: Upgrade to boost 1.56 using the instructions in common.hpp.

---
+++ include/ndnboost/noncopyable.hpp
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2014 Glen Fernandes
+ *
+ * Distributed under the Boost Software License, Version 1.0. (See
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ */
+
+#ifndef NDNBOOST_NONCOPYABLE_HPP
+#define NDNBOOST_NONCOPYABLE_HPP
+
+// The header file at this path is deprecated;
+// use ndnboost/core/noncopyable.hpp instead.
+
+#include <ndnboost/core/noncopyable.hpp>
+
+#endif
