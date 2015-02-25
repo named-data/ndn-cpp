418a301a6093f4058b0595745836367d8085531e
Jeff Thompson <jefft0@remap.ucla.edu>
2015-02-25 09:33:47 -0800

    Name: In equals, check from last to first since the last components are more likely to differ.

--- src/name.cpp
+++ src/name.cpp
@@ -368,7 +368,8 @@ Name::equals(const Name& name) const
   if (components_.size() != name.components_.size())
     return false;
 
-  for (size_t i = 0; i < components_.size(); ++i) {
+  // Check from last to first since the last components are more likely to differ.
+  for (int i = components_.size() - 1; i >= 0; --i) {
     if (*components_[i].getValue() != *name.components_[i].getValue())
       return false;
   }
