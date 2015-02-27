43d542cfc0cb251a14344014d0784df7198fbba1
Jeff Thompson <jefft0@remap.ucla.edu>
2015-02-27 02:11:37 -0800

    encoding: Added initial C ndn_Tlv0_1_1WireFormat.

---
+++ src/c/encoding/tlv-0_1_1-wire-format.h
@@ -0,0 +1,77 @@
+/**
+ * Copyright (C) 2015 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#ifndef NDN_TLV_0_1_1_WIRE_FORMAT_H
+#define NDN_TLV_0_1_1_WIRE_FORMAT_H
+
+#include <ndn-cpp/c/errors.h>
+
+struct ndn_Data;
+struct ndn_DynamicUInt8Array;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Encode the data packet as NDN-TLV.
+ * @param data Pointer to the data object to encode.
+ * @param signedPortionBeginOffset Return the offset in the encoding of the 
+ * beginning of the signed portion. If you are not encoding in order to sign,
+ * you can ignore this returned value.
+ * @param signedPortionEndOffset Return the offset in the encoding of the end of 
+ * the signed portion. If you are not encoding in order to sign, you can ignore
+ * this returned value.
+ * @param output A pointer to a ndn_DynamicUInt8Array struct which receives the
+ * encoded output.  The struct must remain valid during the entire life of this
+ * ndn_TlvEncoder. If the output->realloc function pointer is null, its array
+ * must be large enough to receive the entire encoding.
+ * @param encodingLength Set encodingLength to the length of the encoded output.
+ * @return 0 for success, else an error code.
+ */
+ndn_Error
+ndn_Tlv0_1_1WireFormat_encodeData
+  (struct ndn_Data *data, size_t *signedPortionBeginOffset,
+   size_t *signedPortionEndOffset, struct ndn_DynamicUInt8Array *output,
+   size_t *encodingLength);
+
+/**
+ * Decode input as a data packet in NDN-TLV and set the fields in the data object.
+ * @param data Pointer to the data object whose fields are updated.
+ * @param input A pointer to the input buffer to decode.
+ * @param inputLength The number of bytes in input.
+ * @param signedPortionBeginOffset Return the offset in the input buffer of the
+ * beginning of the signed portion. If you are not decoding in order to verify,
+ * you can ignore this returned value.
+ * @param signedPortionEndOffset Return the offset in the input buffer of the end
+ * of the signed portion. If you are not decoding in order to verify, you can
+ * ignore this returned value.
+ * @return 0 for success, else an error code.
+ */
+ndn_Error
+ndn_Tlv0_1_1WireFormat_decodeData
+  (struct ndn_Data *data, const uint8_t *input, size_t inputLength, 
+   size_t *signedPortionBeginOffset, size_t *signedPortionEndOffset);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
