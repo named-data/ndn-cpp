17556203b17dc502a4e2224a57d4e4363fe89fb2
Jeff Thompson <jefft0@remap.ucla.edu>
2015-02-27 12:16:43 -0800

    encoding: Explicitly convert int to big endian array in TLV encode and decode if NDN_CPP_HAVE_ENDIAN_H is 0.

--- src/c/encoding/tlv/tlv-decoder.c
+++ src/c/encoding/tlv/tlv-decoder.c
@@ -31,9 +31,14 @@ ndn_TlvDecoder_readExtendedVarNumber(struct ndn_TlvDecoder *self, unsigned int f
     if (self->offset + 2 > self->inputLength)
       return NDN_ERROR_read_past_the_end_of_the_input;
 
+#if NDN_CPP_HAVE_ENDIAN_H
     // kind of dangerous... but should be efficient.
     beValue = *(uint16_t *)(self->input + self->offset);
     *varNumber = (uint64_t)be16toh(beValue);
+#else
+    *varNumber = (((uint16_t)self->input[self->offset]) << 8) +
+                  ((uint16_t)self->input[self->offset + 1]);
+#endif
     self->offset += 2;
   }
   else if (firstOctet == 254) {
@@ -41,9 +46,16 @@ ndn_TlvDecoder_readExtendedVarNumber(struct ndn_TlvDecoder *self, unsigned int f
     if (self->offset + 4 > self->inputLength)
       return NDN_ERROR_read_past_the_end_of_the_input;
 
+#if NDN_CPP_HAVE_ENDIAN_H
     // kind of dangerous... but should be efficient.
     beValue = *(uint32_t *)(self->input + self->offset);
     *varNumber = (uint64_t)be32toh(beValue);
+#else
+    *varNumber = (((uint32_t)self->input[self->offset]) << 24) +
+                 (((uint32_t)self->input[self->offset + 1]) << 16) +
+                 (((uint32_t)self->input[self->offset + 2]) << 8) +
+                  ((uint32_t)self->input[self->offset + 3]);
+#endif
     self->offset += 4;
   }
   else {
@@ -52,9 +64,20 @@ ndn_TlvDecoder_readExtendedVarNumber(struct ndn_TlvDecoder *self, unsigned int f
     if (self->offset + 8 > self->inputLength)
       return NDN_ERROR_read_past_the_end_of_the_input;
 
+#if NDN_CPP_HAVE_ENDIAN_H
     // kind of dangerous... but should be efficient.
     beValue = *(uint64_t *)(self->input + self->offset);
     *varNumber = be64toh(beValue);
+#else
+    *varNumber = (((uint64_t)self->input[self->offset]) << 56) +
+                 (((uint64_t)self->input[self->offset + 1]) << 48) +
+                 (((uint64_t)self->input[self->offset + 2]) << 40) +
+                 (((uint64_t)self->input[self->offset + 3]) << 32) +
+                 (((uint64_t)self->input[self->offset + 4]) << 24) +
+                 (((uint64_t)self->input[self->offset + 5]) << 16) +
+                 (((uint64_t)self->input[self->offset + 6]) << 8) +
+                  ((uint64_t)self->input[self->offset + 7]);
+#endif
     self->offset += 8;
   }
 
@@ -96,33 +119,56 @@ ndn_TlvDecoder_readExtendedNonNegativeInteger(struct ndn_TlvDecoder *self, size_
       if (self->offset + 2 > self->inputLength)
         return NDN_ERROR_read_past_the_end_of_the_input;
 
+#if NDN_CPP_HAVE_ENDIAN_H
       {
         // kind of dangerous... but should be efficient.
         uint16_t beValue = *(uint16_t *)(self->input + self->offset);
         *value = (uint64_t)be16toh(beValue);
       }
-      self->offset += 2;
+#else
+      *value = (((uint16_t)self->input[self->offset]) << 8) +
+                ((uint16_t)self->input[self->offset + 1]);
+#endif
+        self->offset += 2;
       return NDN_ERROR_success;
     case 4:
       if (self->offset + 4 > self->inputLength)
         return NDN_ERROR_read_past_the_end_of_the_input;
 
+#if NDN_CPP_HAVE_ENDIAN_H
       {
         // kind of dangerous... but should be efficient.
         uint32_t beValue = *(uint32_t *)(self->input + self->offset);
         *value = (uint64_t)be32toh(beValue);
       }
+#else
+      *value = (((uint32_t)self->input[self->offset]) << 24) +
+               (((uint32_t)self->input[self->offset + 1]) << 16) +
+               (((uint32_t)self->input[self->offset + 2]) << 8) +
+                ((uint32_t)self->input[self->offset + 3]);
+#endif
       self->offset += 4;
       return NDN_ERROR_success;
     case 8:
       if (self->offset + 8 > self->inputLength)
         return NDN_ERROR_read_past_the_end_of_the_input;
 
+#if NDN_CPP_HAVE_ENDIAN_H
       {
         // kind of dangerous... but should be efficient.
         uint64_t beValue = *(uint64_t *)(self->input + self->offset);
         *value = be64toh(beValue);
       }
+#else
+      *value = (((uint64_t)self->input[self->offset]) << 56) +
+               (((uint64_t)self->input[self->offset + 1]) << 48) +
+               (((uint64_t)self->input[self->offset + 2]) << 40) +
+               (((uint64_t)self->input[self->offset + 3]) << 32) +
+               (((uint64_t)self->input[self->offset + 4]) << 24) +
+               (((uint64_t)self->input[self->offset + 5]) << 16) +
+               (((uint64_t)self->input[self->offset + 6]) << 8) +
+                ((uint64_t)self->input[self->offset + 7]);
+#endif
       self->offset += 8;
       return NDN_ERROR_success;
   }
