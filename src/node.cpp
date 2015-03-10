067ac2a2ae8a1fd4a7fd02f58aa05f941345750d
Jeff Thompson <jefft0@remap.ucla.edu>
2015-03-10 11:37:08 -0700

    Node: In expressInterest throw an exception if the encoded interest size exceeds getMaxNdnPacketSize(). Refs #1921.

--- src/node.cpp
+++ src/node.cpp
@@ -208,6 +208,9 @@ Node::expressInterest(const Interest& interest, const OnData& onData, const OnTi
   // Special case: For timeoutPrefix_ we don't actually send the interest.
   if (!timeoutPrefix_.match(interest.getName())) {
     Blob encoding = interest.wireEncode(wireFormat);
+    if (encoding.size() > getMaxNdnPacketSize())
+      throw runtime_error
+        ("The encoded interest size exceeds the maximum limit getMaxNdnPacketSize()");
     transport_->send(*encoding);
   }
 
