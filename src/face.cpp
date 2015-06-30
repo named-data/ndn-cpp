c4c778e0129aeb20cba684acf879a51b322bb926
Jeff Thompson <jefft0@remap.ucla.edu>
2015-06-30 15:41:11 -0700

    face.cpp: Need to include stdexcept.

--- src/face.cpp
+++ src/face.cpp
@@ -22,6 +22,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stdexcept>
 #include "node.hpp"
 #include <ndn-cpp/transport/tcp-transport.hpp>
 #include <ndn-cpp/transport/unix-transport.hpp>
