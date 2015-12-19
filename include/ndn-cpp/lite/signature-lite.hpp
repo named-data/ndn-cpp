bccd671bf99447c6c4639e9cafa05d071a8937bb
Jeff Thompson <jefft0@remap.ucla.edu>
2015-12-19 02:29:59 -0800

    lite (internal): In SignatureLite, make Tlv0_1_1WireFormatLite a friend.

--- include/ndn-cpp/lite/signature-lite.hpp
+++ include/ndn-cpp/lite/signature-lite.hpp
@@ -99,6 +99,9 @@ public:
   upCast(const ndn_Signature& signature) { return *(SignatureLite*)&signature; }
 
 private:
+  // Declare friends who can downcast to the private base.
+  friend class Tlv0_1_1WireFormatLite;
+
   /**
    * Don't allow the copy constructor. Instead use set(const SignatureLite&)
    * which can return an error if there is no more room in the name components
