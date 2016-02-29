5b457379b58e428c9220ee6d28382b342ca02519
Jeff Thompson <jefft0@remap.ucla.edu>
2016-02-29 14:03:00 -0800

    util (internal): Added ConfigFile. Refs #1850.

---
+++ src/util/config-file.cpp
@@ -0,0 +1,114 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2016 Regents of the University of California.
+ * @author: From https://github.com/named-data/ndn-cxx/blob/master/src/util/config-file.cpp
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <ndn-cpp/common.hpp>
+#include "config-file.hpp"
+
+using namespace std;
+
+namespace ndn {
+
+ConfigFile::ConfigFile()
+  : path_(findConfigFile())
+{
+  if (!path_.empty())
+    parse();
+}
+
+ConfigFile::Error::~Error() throw()
+{
+}
+
+string
+ConfigFile::findConfigFile()
+{
+  // Note: We don't use <filesystem> support because it is not "header-only" and
+  // requires linking to libraries.
+  // TODO: Handle non-unix file system paths which don't use '/'.
+  const char* home = getenv("HOME");
+  if (home && *home != '\0') {
+    string homeDir(home);
+    if (homeDir[homeDir.size() - 1] == '/' || homeDir[homeDir.size() - 1] == '\\')
+      // Strip the ending path separator.
+      homeDir.erase(homeDir.size() - 1);
+
+    string filePath = homeDir + '/' + ".ndn/client.conf";
+    ifstream file(filePath.c_str());
+    if (file.good())
+      return filePath;
+  }
+
+#ifdef NDN_CPP_SYSCONFDIR
+  {
+    string sysConfDir(NDN_CPP_SYSCONFDIR);
+    if (sysConfDir[sysConfDir.size() - 1] == '/' || sysConfDir[sysConfDir.size() - 1] == '\\')
+      // Strip the ending path separator.
+      sysConfDir.erase(sysConfDir.size() - 1);
+
+    string filePath = sysConfDir + '/' + "ndn/client.conf";
+    ifstream file(filePath.c_str());
+    if (file.good())
+      return filePath;
+  }
+#endif
+
+  {
+    string filePath("/etc/ndn/client.conf");
+    ifstream file(filePath.c_str());
+    if (file.good())
+      return filePath;
+  }
+
+  return "";
+}
+
+void
+ConfigFile::parse()
+{
+  if (path_.empty())
+    throw Error("ConfigFile::parse: Failed to locate configuration file for parsing");
+
+  ifstream input(path_.c_str());
+  string line;
+  while (getline(input, line)) {
+    ndn_trim(line);
+    if (line.empty() || line[0] == ';')
+      // Skip empty lines and comments.
+      continue;
+
+    size_t iSeparator = line.find('=');
+    if (iSeparator == string::npos)
+      continue;
+
+    string key = line.substr(0, iSeparator);
+    string value = line.substr(iSeparator + 1);
+    ndn_trim(key);
+    ndn_trim(value);
+
+    config_[key] = value;
+  }
+}
+
+}
---
+++ src/util/config-file.hpp
@@ -0,0 +1,113 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2016 Regents of the University of California.
+ * @author: From https://github.com/named-data/ndn-cxx/blob/master/src/util/config-file.hpp
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#ifndef NDN_CONFIG_FILE_HPP
+#define NDN_CONFIG_FILE_HPP
+
+#include <string>
+#include <map>
+#include <exception>
+
+namespace ndn {
+
+class ConfigFile {
+public:
+  /**
+   * Locate, open, and parse a library configuration file.
+   */
+  ConfigFile();
+
+  class Error : public std::exception {
+  public:
+    Error(const std::string& errorMessage) throw()
+    : errorMessage_(errorMessage)
+    {
+    }
+
+    virtual ~Error() throw();
+
+    std::string
+    Msg() { return errorMessage_; }
+
+    virtual const char*
+    what() const throw() { return errorMessage_.c_str(); }
+
+  private:
+    const std::string errorMessage_;
+  };
+
+  /**
+   * Get the value for the key, or a default valud if not found.
+   * @param key The key to search for.
+   * @param defaultValue The default value if the key is not found.
+   * @return The value, or defaultValue if the key is not found.
+   */
+  std::string
+  get(const std::string& key, const std::string& defaultValue) const
+  {
+    std::map<std::string, std::string>::const_iterator found = config_.find(key);
+    if (found != config_.end())
+      return found->second;
+    else
+      return defaultValue;
+  }
+
+  /**
+   * Get the path of the configuration file.
+   * @return The path or an empty string if not found.
+   */
+  const std::string&
+  getPath() const { return path_; }
+
+  /**
+   * Get the configuration key/value pairs.
+   * @return A map of key/value pairs.
+   */
+  const std::map<std::string, std::string>&
+  getParsedConfiguration() const { return config_; }
+
+private:
+  /**
+   * Look for the configuration file in these well-known locations:
+   *
+   * 1. $HOME/.ndn/client.conf
+   * 2. @SYSCONFDIR@/ndn/client.conf
+   * 3. /etc/ndn/client.conf
+   *
+   * @return The path of the config file or an empty string if not found.
+   */
+  static std::string
+  findConfigFile();
+
+  /**
+   * Open path_, parse the configuration file and set config_.
+   */
+  void
+  parse();
+
+  std::string path_;
+  std::map<std::string, std::string> config_;
+};
+
+}
+
+#endif
