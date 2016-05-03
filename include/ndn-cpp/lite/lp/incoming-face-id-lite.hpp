1e7858756a6763e6a46dcd3db9ebc97de5cf3a09
Jeff Thompson <jefft0@remap.ucla.edu>
2016-05-03 12:52:36 -0700

    lite: Added LpPacketLite, IncomingFaceIdLite and NetworkNackLite. Refs #3446.

---
+++ include/ndn-cpp/lite/lp/incoming-face-id-lite.hpp
@@ -0,0 +1,88 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2016 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From ndn-cxx fields.hpp https://github.com/named-data/ndn-cxx/blob/master/src/lp/fields.hpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#ifndef NDN_INCOMING_FACE_ID_LITE_HPP
+#define NDN_INCOMING_FACE_ID_LITE_HPP
+
+#include "../../c/lp/incoming-face-id-types.h"
+
+namespace ndn {
+
+class LpPacketLite;
+
+/**
+ * IncomingFaceIdLite represents the incoming face ID header field in an NDNLPv2
+ * packet.
+ * http://redmine.named-data.net/projects/nfd/wiki/NDNLPv2
+ */
+class IncomingFaceIdLite : private ndn_IncomingFaceId {
+public:
+  /**
+   * Create a IncomingFaceIdLite where all the values are unspecified.
+   */
+  IncomingFaceIdLite();
+
+  /**
+   * Get the incoming face ID value.
+   * @return The face ID value.
+   */
+  uint64_t
+  getFaceId() const { return faceId; }
+
+  /**
+   * Set the face ID value.
+   * @param faceId The incoming face ID value.
+   */
+  void
+  setFaceId(uint64_t faceId) { this->faceId = faceId; }
+
+  /**
+   * Get the first header field in lpPacket which is an IncomingFaceIdLite.
+   * @param lpPacket The ndn_LpPacket struct with the header fields to search.
+   * @return A pointer to the first IncomingFaceIdLite header field, or 0 if not
+   * found.
+   */
+  static const IncomingFaceIdLite*
+  getFirstHeader(const LpPacketLite& lpPacket);
+
+  /**
+   * Upcast the reference to the ndn_IncomingFaceId struct to an
+   * IncomingFaceIdLite.
+   * @param incomingFaceId A reference to the ndn_IncomingFaceId struct.
+   * @return The same reference as IncomingFaceIdLite.
+   */
+  static IncomingFaceIdLite&
+  upCast(ndn_IncomingFaceId& incomingFaceId)
+  {
+    return *(IncomingFaceIdLite*)&incomingFaceId;
+  }
+
+  static const IncomingFaceIdLite&
+  upCast(const ndn_IncomingFaceId& incomingFaceId)
+  {
+    return *(IncomingFaceIdLite*)&incomingFaceId;
+  }
+};
+
+}
+
+#endif
---
+++ include/ndn-cpp/lite/lp/lp-packet-lite.hpp
@@ -0,0 +1,171 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2016 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From ndn-cxx packet.hpp https://github.com/named-data/ndn-cxx/blob/master/src/lp/packet.hpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#ifndef NDN_LPPACKETLITE_HPP
+#define NDN_LPPACKETLITE_HPP
+
+#include "../util/blob-lite.hpp"
+#include "incoming-face-id-lite.hpp"
+#include "../network-nack-lite.hpp"
+#include "../../c/lp/lp-packet-types.h"
+
+namespace ndn {
+
+class LpPacketHeaderFieldLite : private ndn_LpPacketHeaderField {
+public:
+  /**
+   * Get the type of this header field.
+   * @return The header field type.
+   */
+  ndn_LpPacketHeaderFieldType
+  getType() const { return type; }
+
+  /**
+   * Get this field as an IncomingFaceId. This assumes you checked that
+   * getType() is ndn_LpPacketHeaderFieldType_INCOMING_FACE_ID.
+   * @return This field as an IncomingFaceId.
+   */
+  const IncomingFaceIdLite&
+  getIncomingFaceId() const { return IncomingFaceIdLite::upCast(incomingFaceId); }
+
+  IncomingFaceIdLite&
+  getIncomingFaceId() { return IncomingFaceIdLite::upCast(incomingFaceId); }
+
+  /**
+   * Get this field as an NetworkNack. This assumes you checked that
+   * getType() is ndn_LpPacketHeaderFieldType_INCOMING_FACE_ID.
+   * @return This field as an NetworkNack.
+   */
+  const NetworkNackLite&
+  getNetworkNack() const { return NetworkNackLite::upCast(networkNack); }
+
+  NetworkNackLite&
+  getNetworkNack() { return NetworkNackLite::upCast(networkNack); }
+
+  /**
+   * Upcast the reference to the ndn_LpPacketHeaderField struct to an
+   * LpPacketHeaderFieldLite.
+   * @param lpPacketHeaderField A reference to the ndn_LpPacketHeaderField struct.
+   * @return The same reference as LpPacketHeaderFieldLite.
+   */
+  static LpPacketHeaderFieldLite&
+  upCast(ndn_LpPacketHeaderField& lpPacketHeaderField)
+  {
+    return *(LpPacketHeaderFieldLite*)&lpPacketHeaderField;
+  }
+
+  static const LpPacketHeaderFieldLite&
+  upCast(const ndn_LpPacketHeaderField& lpPacketHeaderField)
+  {
+    return *(LpPacketHeaderFieldLite*)&lpPacketHeaderField;
+  }
+};
+
+class LpPacketLite : private ndn_LpPacket {
+public:
+  /**
+   * Create an LpPacket to use the headerFieldsArray.
+   * @param headerFields The pre-allocated array of ndn_LpPacketHeaderField.
+   * This is an array of the underlying ndn_LpPacketHeaderField struct so that
+   * it doesn't run a default constructor unnecessarily.
+   * @param maxHeaderFields The number of elements in the allocated headerFields
+   * array.
+   */
+  LpPacketLite
+    (struct ndn_LpPacketHeaderField* headerFields, size_t maxHeaderFields);
+
+  /**
+   * Get the fragment wire encoding.
+   * @return The wire encoding, or an isNull Blob if not specified.
+   */
+  const BlobLite&
+  getFragmentWireEncoding() const { return BlobLite::upCast(fragmentWireEncoding); }
+
+  /**
+   * Get the number of header fields. This does not include the fragment.
+   * @return The number of header fields.
+   */
+  size_t
+  countHeaderFields() const { return nHeaderFields; }
+
+  /**
+   * Get the header field at the given index.
+   * @param index The index, starting from 0. It is an error if index is greater
+   * to or equal to countHeaderFields().
+   * @return The header field at the index.
+   */
+  const LpPacketHeaderFieldLite&
+  getHeaderField(int index) const
+  {
+    return LpPacketHeaderFieldLite::upCast(headerFields[index]);
+  }
+
+  LpPacketHeaderFieldLite&
+  getHeaderField(int index)
+  {
+    return LpPacketHeaderFieldLite::upCast(headerFields[index]);
+  }
+
+  /**
+   * Set the fragment wire encoding.
+   * @param fragmentWireEncoding The fragment wire encoding or an isNull
+   * BlobLite if not specified.
+   */
+  void
+  setFragmentWireEncoding(const BlobLite& fragmentWireEncoding)
+  {
+    BlobLite::upCast(this->fragmentWireEncoding) = fragmentWireEncoding;
+  }
+
+  /**
+   * Upcast the reference to the ndn_LpPacket struct to an LpPacketLite.
+   * @param lpPacket A reference to the ndn_LpPacket struct.
+   * @return The same reference as LpPacketLite.
+   */
+  static LpPacketLite&
+  upCast(ndn_LpPacket& lpPacket) { return *(LpPacketLite*)&lpPacket; }
+
+  static const LpPacketLite&
+  upCast(const ndn_LpPacket& lpPacket) { return *(LpPacketLite*)&lpPacket; }
+
+private:
+  // Declare friends who can downcast to the private base.
+  friend class Tlv0_1_1WireFormatLite;
+  friend class IncomingFaceIdLite;
+  friend class NetworkNackLite;
+
+  /**
+   * Don't allow the copy constructor. Instead use set(const LpPacketLite&) which
+   * can return an error if there is no more room in the headerFields array.
+   */
+  LpPacketLite(const LpPacketLite& other);
+
+  /**
+   * Don't allow the assignment operator. Instead use set(const LpPacketLite&) which
+   * can return an error if there is no more room in the headerFields array.
+   */
+  LpPacketLite& operator=(const LpPacketLite& other);
+};
+
+}
+
+#endif
