94d5c9f709a411e4aad2c53eb7701cc69f7ca80e
Jeff Thompson <jefft0@remap.ucla.edu>
2016-05-25 15:26:02 -0700

    encrypt: Added EncryptParams.

---
+++ include/ndn-cpp/encrypt/algo/encrypt-params.hpp
@@ -0,0 +1,103 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2016 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From ndn-group-encrypt src/encrypt-params https://github.com/named-data/ndn-group-encrypt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#ifndef NDN_ENCRYPT_PARAMS_HPP
+#define NDN_ENCRYPT_PARAMS_HPP
+
+#include "../../util/blob.hpp"
+#include "../../c/encrypt/algo/encrypt-params-types.h"
+
+namespace ndn {
+
+/**
+ * An EncryptParams holds an algorithm type and other parameters used to encrypt
+ * and decrypt.
+ * @note This class is an experimental feature. The API may change.
+ */
+class EncryptParams {
+public:
+  /**
+   * Create an EncryptParams with the given parameters.
+   * @param algorithmType The algorithm type, or -1 if not specified.
+   * @param initialVectorLength The initial vector length, or 0 if the initial
+   * vector is not specified.
+   */
+  EncryptParams
+    (ndn_EncryptAlgorithmType algorithmType, size_t initialVectorLength);
+
+  /**
+   * Create an EncryptParams with the given algorithmType and an unspecified
+   * initial vector.
+   * @param algorithmType The algorithm type, or -1 if not specified.
+   */
+  EncryptParams(ndn_EncryptAlgorithmType algorithmType)
+  {
+    algorithmType_ = algorithmType;
+  }
+
+  /**
+   * Get the algorithm type.
+   * @return The algorithm type, or -1 if not specified.
+   */
+  ndn_EncryptAlgorithmType
+  getAlgorithmType() const { return algorithmType_; }
+
+  /**
+   * Get the initial vector.
+   * @return The initial vector. If not specified, isNull() is true.
+   */
+  const Blob&
+  getInitialVector() const { return initialVector_; }
+
+  /**
+   * Set the algorithm type.
+   * @param algorithmType The algorithm type. If not specified, set to -1.
+   * @return This EncryptParams so that you can chain calls to update values.
+   */
+  EncryptParams&
+  setAlgorithmType(ndn_EncryptAlgorithmType algorithmType)
+  {
+    algorithmType_ = algorithmType;
+    return *this;
+  }
+
+  /**
+   * Set the initial vector.
+   * @param initialVector The initial vector. If not specified, set to the
+   * default Blob() where isNull() is true.
+   * @return This EncryptParamsLite so that you can chain calls to update values.
+   */
+  EncryptParams&
+  setInitialVector(const Blob& initialVector)
+  {
+    initialVector_ = initialVector;
+    return *this;
+  }
+
+private:
+  ndn_EncryptAlgorithmType algorithmType_;
+  Blob initialVector_;
+};
+
+}
+
+#endif
---
+++ src/encrypt/algo/encrypt-params.cpp
@@ -0,0 +1,43 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2016 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From ndn-group-encrypt src/algo/encrypt-params https://github.com/named-data/ndn-group-encrypt
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#include "../../c/util/crypto.h"
+#include <ndn-cpp/encrypt/algo/encrypt-params.hpp>
+
+using namespace std;
+
+namespace ndn {
+
+EncryptParams::EncryptParams
+  (ndn_EncryptAlgorithmType algorithmType, size_t initialVectorLength)
+{
+  algorithmType_ = algorithmType;
+
+  if (initialVectorLength > 0) {
+    ptr_lib::shared_ptr<vector<uint8_t> > initialVector
+      (new vector<uint8_t>(initialVectorLength));
+    ndn_generateRandomBytes(&initialVector->front(), initialVector->size());
+    initialVector_ = Blob(initialVector, false);
+  }
+}
+
+}
