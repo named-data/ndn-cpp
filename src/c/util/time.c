d8822ef842d9fce6c53c282174524d09391fb4ae
Jeff Thompson <jefft0@remap.ucla.edu>
2016-06-02 12:44:41 -0700

    code style: In C, declare variables at the start of the block.

--- src/c/util/time.c
+++ src/c/util/time.c
@@ -58,22 +58,27 @@ ndn_Error
 ndn_toIsoString(ndn_MillisecondsSince1970 milliseconds, char *isoString)
 {
 #if NDN_CPP_HAVE_GMTIME_SUPPORT
+  double secondsSince1970;
+  char fractionBuffer[10];
+  const char *fraction;
+  time_t seconds;
+  struct tm* gmt;
+
   if (milliseconds < 0)
     return NDN_ERROR_Calendar_time_value_out_of_range;
   else if (milliseconds > 2e14)
     // 2e14 is about the year 8300.  We don't want to go over a 4-digit year.
     return NDN_ERROR_Calendar_time_value_out_of_range;
 
-  double secondsSince1970 = milliseconds / 1000.0;
-  char fractionBuffer[10];
+  secondsSince1970 = milliseconds / 1000.0;
   sprintf(fractionBuffer, "%.06lf", fmod(secondsSince1970, 1.0));
-  const char *fraction = strchr(fractionBuffer, '.');
+  fraction = strchr(fractionBuffer, '.');
   if (!fraction)
     // Don't expect this to happen.
     fraction = ".000000";
 
-  time_t seconds = (time_t)floor(secondsSince1970);
-  struct tm* gmt = gmtime(&seconds);
+  seconds = (time_t)floor(secondsSince1970);
+  gmt = gmtime(&seconds);
   sprintf(isoString, "%04d%02d%02dT%02d%02d%02d%s", 1900 + gmt->tm_year, gmt->tm_mon + 1, gmt->tm_mday,
     gmt->tm_hour, gmt->tm_min, gmt->tm_sec, fraction);
 
@@ -90,10 +95,10 @@ ndn_fromIsoString(const char* isoString, ndn_MillisecondsSince1970 *milliseconds
   // Initialize time zone, etc.
   time_t dummyTime = 0;
   struct tm tm1 = *gmtime(&dummyTime);
+  double seconds;
 
   sscanf(isoString, "%4d%2d%2dT%2d%2d", &tm1.tm_year, &tm1.tm_mon, &tm1.tm_mday, &tm1.tm_hour, &tm1.tm_min);
   // Skip the time past minutes and get the float seconds.
-  double seconds;
   sscanf(isoString + (4 + 2 + 2 + 1 + 2 + 2), "%lf", &seconds);
 
   // tm_year starts from 1900.
