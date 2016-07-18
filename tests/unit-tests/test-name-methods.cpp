07f9174ad929b424966eec6f1a03d7976c3e4bca
Jeff Thompson <jefft0@remap.ucla.edu>
2016-07-18 15:29:32 -0700

    unit-tests: In test-name-methods EncodeDecode, test ImplicitSha256Digest.

--- tests/unit-tests/test-name-methods.cpp
+++ tests/unit-tests/test-name-methods.cpp
@@ -41,6 +41,19 @@ static const uint8_t TEST_NAME[] = {
         0x70,  0x72,  0x65,  0x66,  0x69,  0x78
 };
 
+static const uint8_t TEST_NAME_IMPLICIT_DIGEST[] = {
+  0x7,  0x36, // Name
+    0x8,  0x5, // NameComponent
+        0x6c,  0x6f,  0x63,  0x61,  0x6c,
+    0x8,  0x3, // NameComponent
+        0x6e,  0x64,  0x6e,
+    0x8,  0x6, // NameComponent
+        0x70,  0x72,  0x65,  0x66,  0x69,  0x78,
+    0x01, 0x20, // ImplicitSha256DigestComponent
+      0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+      0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f
+};
+
 class TestNameComponentMethods : public ::testing::Test {
 };
 
@@ -245,6 +258,19 @@ TEST_F(TestNameMethods, EncodeDecode)
   Name decodedName;
   decodedName.wireDecode(Blob(TEST_NAME, sizeof(TEST_NAME)), *TlvWireFormat::get());
   ASSERT_EQ(decodedName, name);
+
+  // Test ImplicitSha256Digest.
+  Name name2("/local/ndn/prefix/sha256digest="
+             "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f");
+
+  Blob encoding2 = name2.wireEncode(*TlvWireFormat::get());
+  ASSERT_TRUE(encoding2.equals
+    (Blob(TEST_NAME_IMPLICIT_DIGEST, sizeof(TEST_NAME_IMPLICIT_DIGEST))));
+
+  Name decodedName2;
+  decodedName2.wireDecode(Blob(TEST_NAME_IMPLICIT_DIGEST, sizeof(TEST_NAME_IMPLICIT_DIGEST)),
+                          *TlvWireFormat::get());
+  ASSERT_EQ(decodedName2, name2);
 }
 
 TEST_F(TestNameMethods, ImplicitSha256Digest)
