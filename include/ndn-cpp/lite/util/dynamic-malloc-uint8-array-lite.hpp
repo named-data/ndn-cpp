475aa1fd2477bafe8c1166e0350cd4c4cbc54882
Jeff Thompson <jefft0@remap.ucla.edu>
2017-04-04 12:37:15 -0700

    lite/util: Added DynamicMallocUInt8ArrayLite.

---
+++ include/ndn-cpp/lite/util/dynamic-malloc-uint8-array-lite.hpp
@@ -0,0 +1,59 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2017 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#ifndef NDN_DYNAMIC_MALLOC_UINT8_ARRAY_LITE_HPP
+#define NDN_DYNAMIC_MALLOC_UINT8_ARRAY_LITE_HPP
+
+#include "dynamic-uint8-array-lite.hpp"
+
+namespace ndn {
+
+/**
+ * A DynamicMallocUInt8ArrayLite extends DynamicUInt8ArrayLite to use malloc and
+ * realloc to resize the array. If your lightweight platform doesn't support
+ * malloc then don't link to this file, and use DynamicUInt8ArrayLite directly
+ * with a fixed-size array.
+ */
+class DynamicMallocUInt8ArrayLite : public DynamicUInt8ArrayLite {
+public:
+  /**
+   * Create a DynamicMallocUInt8ArrayLite with a buffer of the initial length.
+   * @param initialLength The initial size of the malloced array. This may be 0.
+   */
+  DynamicMallocUInt8ArrayLite(size_t initialLength);
+
+private:
+  /**
+   * Implement the static realloc function using ::realloc.
+   * @param self A pointer to this object.
+   * @param array Should be the front of the vector.
+   * @param length The new length for the vector.
+   * @return The front of the allocated vector.
+   */
+  static uint8_t*
+  realloc(struct ::ndn_DynamicUInt8Array *self, uint8_t *array, size_t length);
+
+  uint8_t *mallocArray_;
+};
+
+}
+
+#endif
---
+++ src/lite/util/dynamic-malloc-uint8-array-lite.cpp
@@ -0,0 +1,53 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2017 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#include <stdlib.h>
+#include <ndn-cpp/lite/util/dynamic-malloc-uint8-array-lite.hpp>
+
+namespace ndn {
+
+DynamicMallocUInt8ArrayLite::DynamicMallocUInt8ArrayLite(size_t initialLength)
+: DynamicUInt8ArrayLite(0, 0, &DynamicMallocUInt8ArrayLite::realloc)
+{
+  if (initialLength <= 0)
+    // Some implementations don't like malloc(0), so increase.
+    initialLength = 1;
+
+  mallocArray_ = (uint8_t*)::malloc(initialLength);
+  setArrayAndLength(mallocArray_, initialLength);
+}
+
+uint8_t*
+DynamicMallocUInt8ArrayLite::realloc
+  (struct ::ndn_DynamicUInt8Array *self, uint8_t *array, size_t length)
+{
+  DynamicMallocUInt8ArrayLite *thisObject = (DynamicMallocUInt8ArrayLite *)self;
+
+  if (array != thisObject->mallocArray_)
+    // We don't expect this to ever happen. The caller didn't pass the array
+    // from this object.
+    return 0;
+
+  thisObject->mallocArray_ = (uint8_t*)::realloc(array, length);
+  return thisObject->mallocArray_;
+}
+
+}
