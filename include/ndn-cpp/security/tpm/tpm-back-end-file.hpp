76dc2ecb26b65961ce0bfb9540537cec82749041
Jeff Thompson <jefft0@remap.ucla.edu>
2017-05-04 09:35:30 -0700

    security: Added TpmBackEndFile. Refs #4045.

---
+++ include/ndn-cpp/security/tpm/tpm-back-end-file.hpp
@@ -0,0 +1,114 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2017 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From ndn-cxx security https://github.com/named-data/ndn-cxx/blob/master/src/security/tpm/back-end-file.hpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#ifndef NDN_TPM_BACK_END_FILE_HPP
+#define NDN_TPM_BACK_END_FILE_HPP
+
+#include "tpm-back-end.hpp"
+
+namespace ndn {
+
+class TpmPrivateKey;
+
+/**
+ * TpmBackEndMemory extends TpmBackEnd to implement a TPM back-end using
+ * on-disk file storage. In this TPM, each private key is stored in a separate
+ * file with permission 0400, i.e., owner read-only.  The key is stored in
+ * PKCS #1 format in base64 encoding.
+ */
+class TpmBackEndFile : public TpmBackEnd {
+public:
+  /**
+   * Create a TpmBackEndFile to use the given path to store files.
+   * @param locationPath (optional) The path of the directory to store private
+   * keys. If omitted use $HOME/.ndn/ndnsec-key-file .
+   */
+  TpmBackEndFile(const std::string& locationPath = "");
+
+  static std::string
+  getScheme() { return "tpm-file"; }
+
+private:
+  /**
+   * Check if the key with name keyName exists in the TPM.
+   * @param The name of the key.
+   * @return True if the key exists.
+   */
+  virtual bool
+  doHasKey(const Name& keyName) const;
+
+  /**
+   * Get the handle of the key with name keyName.
+   * @param The name of the key.
+   * @return The handle of the key, or null if the key does not exist.
+   */
+  virtual ptr_lib::shared_ptr<TpmKeyHandle>
+  doGetKeyHandle(const Name& keyName) const;
+
+  /**
+   * Create a key for identityName according to params. The created key is
+   * named as: /<identityName>/[keyId]/KEY . The key name is set in the returned
+   * TpmKeyHandle.
+   * @return The handle of the created key.
+   * @throw TpmBackEnd::Error if the key cannot be created.
+   */
+  virtual ptr_lib::shared_ptr<TpmKeyHandle>
+  doCreateKey(const Name& identityName, const KeyParams& params);
+
+  /**
+   * Delete the key with name keyName. If the key doesn't exist, do nothing.
+   * @throw TpmBackEnd::Error if the deletion fails.
+   */
+  virtual void
+  doDeleteKey(const Name& keyName);
+
+  /**
+   * Load the private key with name keyName from the key file directory.
+   * @param keyName The name of the key.
+   * @return The key loaded into a TpmPrivateKey.
+   */
+  ptr_lib::shared_ptr<TpmPrivateKey>
+  loadKey(const Name& keyName) const;
+
+  /**
+   * Save the private key using keyName into the key file directory.
+   * @param keyName The name of the key.
+   * @param key The private key to save.
+   */
+  void
+  saveKey(const Name& keyName, ptr_lib::shared_ptr<TpmPrivateKey> key);
+
+  /**
+   * Get the file path for the keyName, which is keyStorePath_ + "/" +
+   * hex(sha256(keyName-wire-encoding)) + ".privkey" .
+   * @param keyName The name of the key.
+   * @return The file path for the key.
+   */
+  std::string
+  toFilePath(const Name& keyName) const;
+
+  std::string keyStorePath_;
+};
+
+}
+
+#endif
---
+++ src/security/tpm/tpm-back-end-file.cpp
@@ -0,0 +1,164 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2017 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream>
+#include <sys/stat.h>
+#include "../../encoding/base64.hpp"
+#include <ndn-cpp/lite/util/crypto-lite.hpp>
+#include <ndn-cpp/security/tpm/tpm-private-key.hpp>
+#include <ndn-cpp/security/tpm/tpm-key-handle-memory.hpp>
+#include <ndn-cpp/security/tpm/tpm-back-end-file.hpp>
+
+using namespace std;
+
+namespace ndn {
+
+TpmBackEndFile::TpmBackEndFile(const string& locationPath)
+{
+  if (locationPath != "") {
+    keyStorePath_ = locationPath;
+    if (keyStorePath_[keyStorePath_.size() - 1] == '/' ||
+        keyStorePath_[keyStorePath_.size() - 1] == '\\')
+      // Strip the ending path separator.
+      keyStorePath_.erase(keyStorePath_.size() - 1);
+  }
+  else {
+    // Note: We don't use <filesystem> support because it is not "header-only"
+    // and requires linking to libraries.
+    const char* home = getenv("HOME");
+    if (!home || *home == '\0')
+      // Don't expect this to happen;
+      home = ".";
+    string homeDir(home);
+    if (homeDir[homeDir.size() - 1] == '/' || homeDir[homeDir.size() - 1] == '\\')
+      // Strip the ending path separator.
+      homeDir.erase(homeDir.size() - 1);
+
+    // TODO: Handle non-unix file systems which don't use "/".
+    keyStorePath_ = homeDir + "/.ndn/ndnsec-key-file";
+  }
+
+  // TODO: Handle non-unix file systems which don't have "mkdir -p".
+  ::system(("mkdir -p \"" + keyStorePath_ + "\"").c_str());
+}
+
+bool
+TpmBackEndFile::doHasKey(const Name& keyName) const
+{
+  {
+    ifstream file(toFilePath(keyName).c_str());
+    if (!file.good())
+      return false;
+  }
+
+  try {
+    loadKey(keyName);
+    return true;
+  }
+  catch (const runtime_error&) {
+    return false;
+  }
+}
+
+ptr_lib::shared_ptr<TpmKeyHandle>
+TpmBackEndFile::doGetKeyHandle(const Name& keyName) const
+{
+  if (!doHasKey(keyName))
+    return ptr_lib::shared_ptr<TpmKeyHandle>();
+
+  return ptr_lib::make_shared<TpmKeyHandleMemory>(loadKey(keyName));
+}
+
+ptr_lib::shared_ptr<TpmKeyHandle>
+TpmBackEndFile::doCreateKey(const Name& identityName, const KeyParams& params)
+{
+  ptr_lib::shared_ptr<TpmPrivateKey> key = TpmPrivateKey::generatePrivateKey
+    (params);
+  ptr_lib::shared_ptr<TpmKeyHandle> keyHandle(new TpmKeyHandleMemory(key));
+
+  setKeyName(*keyHandle, identityName, params);
+
+  try {
+    saveKey(keyHandle->getKeyName(), key);
+    return keyHandle;
+  }
+  catch (const runtime_error& e) {
+    throw Error(string("Cannot write the key to disk: ") + e.what());
+  }
+}
+
+void
+TpmBackEndFile::doDeleteKey(const Name& keyName)
+{
+  string keyPath = toFilePath(keyName);
+
+  {
+    ifstream file(keyPath.c_str());
+    if (!file.good())
+      // Already removed.
+      return;
+  }
+
+  if (remove(keyPath.c_str()) != 0)
+    throw Error("Cannot delete the key");
+}
+
+ptr_lib::shared_ptr<TpmPrivateKey>
+TpmBackEndFile::loadKey(const Name& keyName) const
+{
+  ptr_lib::shared_ptr<TpmPrivateKey> key(new TpmPrivateKey());
+  ifstream file(toFilePath(keyName).c_str());
+  stringstream base64;
+  base64 << file.rdbuf();
+  vector<uint8_t> pkcs;
+  fromBase64(base64.str(), pkcs);
+
+  key->loadPkcs1(&pkcs.front(), pkcs.size());
+  return key;
+}
+
+void
+TpmBackEndFile::saveKey(const Name& keyName, ptr_lib::shared_ptr<TpmPrivateKey> key)
+{
+  string filePath = toFilePath(keyName);
+  ofstream file(filePath.c_str());
+  file << toBase64(*key->toPkcs1(), true);
+
+  // Set the file permissions.
+#if !defined(_WIN32) // Windows doesn't have Unix group permissions.
+  ::chmod(filePath.c_str(), S_IRUSR);
+#endif
+}
+
+string
+TpmBackEndFile::toFilePath(const Name& keyName) const
+{
+  Blob keyEncoding = keyName.wireEncode();
+  uint8_t digest[ndn_SHA256_DIGEST_SIZE];
+  CryptoLite::digestSha256(keyEncoding, digest);
+
+  return keyStorePath_ + "/" + Blob(digest, sizeof(digest)).toHex() + ".privkey";
+}
+
+}
