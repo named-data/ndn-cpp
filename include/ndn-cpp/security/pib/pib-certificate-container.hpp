2b4b53d13d6c929770cc5b6bc61267351480cbe5
Jeff Thompson <jefft0@remap.ucla.edu>
2017-05-30 09:07:31 -0700

    security (minor): In PibCertificateContainer::isConsistent, updated the doc comment.

--- include/ndn-cpp/security/pib/pib-certificate-container.hpp
+++ include/ndn-cpp/security/pib/pib-certificate-container.hpp
@@ -82,6 +82,7 @@ public:
 
   /**
    * Check if the container is consistent with the backend storage.
+   * @return True if the container is consistent, false otherwise.
    * @note This method is heavy-weight and should be used in a debugging mode
    * only.
    */
