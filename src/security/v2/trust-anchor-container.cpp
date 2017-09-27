3e5f498a52928670e43fe5c9dcd668e9d430ac88
Jeff Thompson <jefft0@remap.ucla.edu>
2017-09-27 13:46:00 -0700

    security: Added TrustAnchorContainer and TrustAnchorGroup. Refs #4046.

---
+++ src/security/v2/trust-anchor-container.cpp
@@ -0,0 +1,133 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2017 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From ndn-cxx security https://github.com/named-data/ndn-cxx/blob/master/src/security/v2/trust-anchor-container.cpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#include <ndn-cpp/interest.hpp>
+#include <ndn-cpp/security/v2/trust-anchor-container.hpp>
+
+using namespace std;
+
+namespace ndn {
+
+void
+TrustAnchorContainer::insert
+  (const string& groupId, const CertificateV2& certificate)
+{
+  ptr_lib::shared_ptr<TrustAnchorGroup> group;
+  std::map<string, ptr_lib::shared_ptr<TrustAnchorGroup> >::iterator groupIt =
+    groups_.find(groupId);
+  if (groupIt == groups_.end()) {
+    group.reset(new StaticTrustAnchorGroup(anchors_, groupId));
+    groups_[groupId] = group;
+  }
+  else
+    group = groupIt->second;
+
+  StaticTrustAnchorGroup* staticGroup = dynamic_cast<StaticTrustAnchorGroup*>
+    (group.get());
+  if (!staticGroup)
+    throw Error
+      ("Cannot add a static anchor to the non-static anchor group " + groupId);
+
+  staticGroup->add(certificate);
+}
+
+void
+TrustAnchorContainer::insert
+  (const string& groupId, const string& path, Milliseconds refreshPeriod,
+   bool isDirectory)
+{
+  if (groups_.count(groupId) != 0)
+    throw Error("Cannot create the dynamic group, because group " + groupId +
+      " already exists");
+
+  groups_[groupId] = ptr_lib::make_shared<DynamicTrustAnchorGroup>
+    (anchors_, groupId, path, refreshPeriod, isDirectory);
+}
+
+ptr_lib::shared_ptr<CertificateV2>
+TrustAnchorContainer::find(const Name& keyName) const
+{
+  const_cast<TrustAnchorContainer*>(this)->refresh();
+
+  map<Name, ptr_lib::shared_ptr<CertificateV2> >::const_iterator it =
+    anchors_.anchorsByName_.lower_bound(keyName);
+  if (it ==  anchors_.anchorsByName_.end() ||
+      !keyName.isPrefixOf(it->second->getName()))
+    return ptr_lib::shared_ptr<CertificateV2>();
+  return it->second;
+}
+
+ptr_lib::shared_ptr<CertificateV2>
+TrustAnchorContainer::find(const Interest& interest) const
+{
+  const_cast<TrustAnchorContainer*>(this)->refresh();
+
+  for (map<Name, ptr_lib::shared_ptr<CertificateV2> >::const_iterator it =
+         anchors_.anchorsByName_.lower_bound(interest.getName());
+       it != anchors_.anchorsByName_.end() && 
+         interest.getName().isPrefixOf(it->second->getName());
+       ++it) {
+    if (interest.matchesData(*it->second))
+      return it->second;
+  }
+
+  return ptr_lib::shared_ptr<CertificateV2>();
+}
+
+TrustAnchorGroup&
+TrustAnchorContainer::getGroup(const string& groupId)
+{
+  map<string, ptr_lib::shared_ptr<TrustAnchorGroup> >::iterator group =
+    groups_.find(groupId);
+  if (group == groups_.end())
+    throw Error("Trust anchor group " + groupId + " does not exist");
+
+  return *group->second;
+}
+
+void
+TrustAnchorContainer::refresh()
+{
+  for (map<string, ptr_lib::shared_ptr<TrustAnchorGroup> >::iterator it =
+       groups_.begin(); it != groups_.end(); ++it)
+    it->second->refresh();
+}
+
+TrustAnchorContainer::AnchorContainer::~AnchorContainer()
+{
+}
+
+void
+TrustAnchorContainer::AnchorContainer::add(const CertificateV2& certificate)
+{
+  ptr_lib::shared_ptr<CertificateV2> certificateCopy
+    (new CertificateV2(certificate));
+  anchorsByName_[certificateCopy->getName()] = certificateCopy;
+}
+
+void
+TrustAnchorContainer::AnchorContainer::remove(const Name& certificateName)
+{
+  anchorsByName_.erase(certificateName);
+}
+
+}
---
+++ src/security/v2/trust-anchor-group.cpp
@@ -0,0 +1,166 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2017 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From ndn-cxx security https://github.com/named-data/ndn-cxx/blob/master/src/security/v2/trust-anchor-group.cpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fstream>
+#include <sstream>
+#include <ndn-cpp/util/logging.hpp>
+#include "../../encoding/base64.hpp"
+#include <ndn-cpp/security/v2/trust-anchor-group.hpp>
+
+INIT_LOGGER("ndn.TrustAnchorGroup");
+
+using namespace std;
+
+namespace ndn {
+
+CertificateContainerInterface::~CertificateContainerInterface()
+{
+}
+
+TrustAnchorGroup::~TrustAnchorGroup()
+{
+}
+
+void
+TrustAnchorGroup::refresh()
+{
+  // The base method does nothing.
+}
+
+ptr_lib::shared_ptr<CertificateV2>
+TrustAnchorGroup::readCertificate(const string& filePath)
+{
+  ifstream certificateFile(filePath.c_str());
+  if (!certificateFile.good())
+    return ptr_lib::shared_ptr<CertificateV2>();
+
+  stringstream encodedData;
+  encodedData << certificateFile.rdbuf();
+
+  // Use a vector in a shared_ptr so we can make it a Blob without copying.
+  ptr_lib::shared_ptr<vector<uint8_t> > decodedData(new vector<uint8_t>());
+  fromBase64(encodedData.str(), *decodedData);
+
+  ptr_lib::shared_ptr<CertificateV2> result(new CertificateV2());
+  try {
+    result->wireDecode(Blob(decodedData, false));
+    return result;
+  } catch (...) {
+    return ptr_lib::shared_ptr<CertificateV2>();
+  }
+}
+
+void
+StaticTrustAnchorGroup::add(const CertificateV2& certificate)
+{
+  if (anchorNames_.count(certificate.getName()) != 0)
+    return;
+
+  // This copies the certificate name.
+  anchorNames_.insert(certificate.getName());
+  // This copies the certificate.
+  certificates_.add(certificate);
+}
+
+void
+StaticTrustAnchorGroup::remove(const Name& certificateName)
+{
+  anchorNames_.erase(certificateName);
+  certificates_.remove(certificateName);
+}
+
+DynamicTrustAnchorGroup::DynamicTrustAnchorGroup
+  (CertificateContainerInterface& certificateContainer, const string& id,
+   const string& path, Milliseconds refreshPeriod, bool isDirectory)
+: TrustAnchorGroup(certificateContainer, id),
+  isDirectory_(isDirectory),
+  path_(path),
+  refreshPeriod_(refreshPeriod),
+  expireTime_(0)
+{
+  if (refreshPeriod <= 0)
+    throw runtime_error("Refresh period for the dynamic group must be positive");
+
+  _LOG_TRACE("Create a dynamic trust anchor group " << id << " for file/dir " <<
+    path << " with refresh time " << refreshPeriod);
+  refresh();
+}
+
+void
+DynamicTrustAnchorGroup::refresh()
+{
+  Milliseconds now = ndn_getNowMilliseconds();
+  if (expireTime_ > now)
+    return;
+
+  expireTime_ = now + refreshPeriod_;
+  _LOG_TRACE("Reloading the dynamic trust anchor group");
+
+  // Save a copy of anchorNames_ .
+  set<Name> oldAnchorNames = anchorNames_;
+
+  if (!isDirectory_)
+    loadCertificate(path_, oldAnchorNames);
+  else {
+    DIR *directory = ::opendir(path_.c_str());
+    if (directory != NULL) {
+      struct dirent *entry;
+      while ((entry = ::readdir(directory)) != NULL) {
+        // TODO: Handle non-unix file systems which don't have stat.
+        string filePath = path_ + '/' + entry->d_name;
+        struct stat fileStat;
+        if (::stat(filePath.c_str(), &fileStat) != -1 &&
+            S_ISREG(fileStat.st_mode))
+          loadCertificate(filePath, oldAnchorNames);
+      }
+
+      ::closedir(directory);
+    }
+  }
+
+  // Remove old certificates.
+  for (set<Name>::iterator name = oldAnchorNames.begin();
+       name != oldAnchorNames.end(); ++name) {
+    anchorNames_.erase(*name);
+    certificates_.remove(*name);
+  }
+}
+
+void
+DynamicTrustAnchorGroup::loadCertificate
+  (const string& file, set<Name>& oldAnchorNames)
+{
+  ptr_lib::shared_ptr<CertificateV2> certificate = readCertificate(file);
+  if (certificate) {
+    if (anchorNames_.count(certificate->getName()) == 0) {
+      anchorNames_.insert(certificate->getName());
+      certificates_.add(*certificate);
+    }
+    else
+      oldAnchorNames.erase(certificate->getName());
+  }
+}
+
+}
