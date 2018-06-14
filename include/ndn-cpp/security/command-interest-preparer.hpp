d73e737584640d832c53f57a2c5148e8cc8385db
Jeff Thompson <jefft0@remap.ucla.edu>
2018-06-14 04:18:55 -0700

    security: Added CommandInterestPreparer.

---
+++ include/ndn-cpp/security/command-interest-preparer.hpp
@@ -0,0 +1,77 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2018 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From ndn-cxx security https://github.com/named-data/ndn-cxx/blob/master/src/security/command-interest-signer.hpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#ifndef NDN_COMMAND_INTEREST_PREPARER_HPP
+#define NDN_COMMAND_INTEREST_PREPARER_HPP
+
+#include "../interest.hpp"
+
+namespace ndn {
+
+/**
+ * A CommandInterestPreparer keeps track of a timestamp and prepares a command
+ * interest by adding a timestamp and nonce to the name of an Interest. This
+ * class is primarily designed to be used by the CommandInterestSigner, but can
+ * also be using in an application that defines custom signing methods not
+ * supported by the KeyChain (such as HMAC-SHA1). See the Command Interest
+ * documentation:
+ * https://redmine.named-data.net/projects/ndn-cxx/wiki/CommandInterest
+ */
+class CommandInterestPreparer {
+public:
+  /**
+   * Create a CommandInterestPreparer and initialize the timestamp to now.
+   */
+  CommandInterestPreparer();
+
+  /**
+   * Append a timestamp component and a random nonce component to interest's
+   * name. This ensures that the timestamp is greater than the timestamp used in
+   * the previous call.
+   * @param interest The interest whose name is append with components.
+   * @param wireFormat (optional) A WireFormat object used to encode the
+   * SignatureInfo. If omitted, use WireFormat getDefaultWireFormat().
+   */
+  void
+  prepareCommandInterestName
+    (Interest& interest,
+     WireFormat& wireFormat = *WireFormat::getDefaultWireFormat());
+
+  /**
+   * Set the offset for when prepareCommandInterestName() gets the current time,
+   * which should only be used for testing.
+   * @param nowOffsetMilliseconds The offset in milliseconds.
+   */
+  void
+  setNowOffsetMilliseconds_(Milliseconds nowOffsetMilliseconds)
+  {
+    nowOffsetMilliseconds_ = nowOffsetMilliseconds;
+  }
+
+private:
+  MillisecondsSince1970 lastUsedTimestamp_;
+  Milliseconds nowOffsetMilliseconds_;
+};
+
+}
+
+#endif
---
+++ src/security/command-interest-preparer.cpp
@@ -0,0 +1,66 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2018 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From ndn-cxx security https://github.com/named-data/ndn-cxx/blob/master/src/security/command-interest-signer.hpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#include <math.h>
+#include <ndn-cpp/lite/util/crypto-lite.hpp>
+#include "../c/util/time.h"
+#include "../encoding/tlv-encoder.hpp"
+#include <ndn-cpp/security/command-interest-preparer.hpp>
+
+using namespace std;
+
+namespace ndn {
+
+CommandInterestPreparer::CommandInterestPreparer()
+: lastUsedTimestamp_(::round(ndn_getNowMilliseconds())),
+  nowOffsetMilliseconds_(0)
+{}
+
+void
+CommandInterestPreparer::prepareCommandInterestName
+  (Interest& interest, WireFormat& wireFormat)
+{
+  // nowOffsetMilliseconds_ is only used for testing.
+  MillisecondsSince1970 now = ndn_getNowMilliseconds() + nowOffsetMilliseconds_;
+  MillisecondsSince1970 timestamp = ::round(now);
+  while (timestamp <= lastUsedTimestamp_)
+    timestamp += 1.0;
+
+  // Update the timestamp now. In the small chance that signing fails, it just
+  // means that we have bumped the timestamp.
+  lastUsedTimestamp_ = timestamp;
+
+  // The timestamp is encoded as a TLV nonNegativeInteger.
+  TlvEncoder encoder(8);
+  encoder.writeNonNegativeInteger((uint64_t)timestamp);
+  interest.getName().append(Blob(encoder.finish()));
+
+  // The random value is a TLV nonNegativeInteger too, but we know it is 8 bytes,
+  //   so we don't need to call the nonNegativeInteger encoder.
+  uint8_t randomBuffer[8];
+  ndn_Error error;
+  if ((error = CryptoLite::generateRandomBytes(randomBuffer, sizeof(randomBuffer))))
+    throw runtime_error(ndn_getErrorString(error));
+  interest.getName().append(randomBuffer, sizeof(randomBuffer));
+}
+
+}
