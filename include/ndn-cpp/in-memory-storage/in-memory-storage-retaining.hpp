0aec66f0e8795f88c38fa945437b87b2022939e2
Jeff Thompson <jefft0@remap.ucla.edu>
2018-10-26 13:59:20 -0700

    Added InMemoryStorageRetaining.

---
+++ include/ndn-cpp/in-memory-storage/in-memory-storage-retaining.hpp
@@ -0,0 +1,80 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2018 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#ifndef NDN_IN_MEMORY_STORAGE_RETAINING_HPP
+#define NDN_IN_MEMORY_STORAGE_RETAINING_HPP
+
+#include <map>
+#include "../interest.hpp"
+#include "../data.hpp"
+
+namespace ndn {
+
+/**
+ * InMemoryStorageRetaining provides an application cache with in-memory
+ * storage, of which no eviction policy will be employed. Entries will only be
+ * evicted by explicit application control.
+ * Note: In ndn-cxx, this class is called InMemoryStoragePersistent, but
+ * "persistent" misleadingly sounds like persistent on-disk storage.
+ */
+class InMemoryStorageRetaining {
+public:
+  /**
+   * Insert a Data packet. If a Data packet with the same name, including the
+   * implicit digest, already exists, replace it.
+   * @param data The packet to insert, which is copied.
+   */
+  void
+  insert(const Data& data)
+  {
+    cache_[*data.getFullName()] = ptr_lib::make_shared<Data>(data);
+  }
+
+  /**
+   * Find the best match Data for an Interest.
+   * @param interest The Interest with the Name of the Data packet to find.
+   * @return The best match if any, otherwise null. You should not modify the
+   * returned object. If you need to modify it then you must make a copy.
+   */
+  ptr_lib::shared_ptr<Data>
+  find(const Interest& interest);
+
+  /**
+   * Get the number of packets stored in the in-memory storage.
+   * @return The number of packets.
+   */
+  size_t
+  size() { return cache_.size(); }
+
+  /**
+   * Get the the storage cache, which should only be used for testing.
+   * @return The storage cache.
+   */
+  const std::map<Name, ptr_lib::shared_ptr<Data> >&
+  getCache_() { return cache_; }
+
+private:
+  std::map<Name, ptr_lib::shared_ptr<Data> > cache_;
+};
+
+}
+
+#endif
---
+++ src/in-memory-storage/in-memory-storage-retaining.cpp
@@ -0,0 +1,41 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2018 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#include <ndn-cpp/in-memory-storage/in-memory-storage-retaining.hpp>
+
+using namespace std;
+
+namespace ndn {
+
+ptr_lib::shared_ptr<Data>
+InMemoryStorageRetaining::find(const Interest& interest)
+{
+  for (map<Name, ptr_lib::shared_ptr<Data> >::iterator i = cache_.begin();
+       i != cache_.end(); ++i) {
+    // Debug: Check selectors, especially CanBePrefix.
+    if (interest.getName().isPrefixOf(i->first))
+      return i->second;
+  }
+
+  return ptr_lib::shared_ptr<Data>();
+}
+
+}
