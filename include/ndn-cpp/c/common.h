71dbcf41b8e0acabe37d596d46759d4fa9647990
Jeff Thompson <jefft0@remap.ucla.edu>
2018-10-31 12:10:50 -0700

    encrypt: Added ndn_AES_128_KEY_LENGTH.

--- include/ndn-cpp/c/common.h
+++ include/ndn-cpp/c/common.h
@@ -79,6 +79,12 @@ static const size_t ndn_SHA256_DIGEST_SIZE = 32;
 static const size_t ndn_AES_BLOCK_LENGTH = 16;
 
 /**
+ * The key size in bytes for the AES 128 algorithm. We define this separately
+ * so that we don't have to include the openssl header everywhere.
+ */
+static const size_t ndn_AES_128_KEY_LENGTH = 16;
+
+/**
  * The key size in bytes for the DES EDE3 algorithm. We define this separately
  * so that we don't have to include the openssl header everywhere.
  */
