f9cfa906546cdf2b5a0ad23416e4f1a4dc3e83b9
Jeff Thompson <jefft0@remap.ucla.edu>
2019-01-30 12:34:04 -0800

    sync: Added InvertibleBloomLookupTable.

---
+++ src/sync/detail/invertible-bloom-lookup-table.cpp
@@ -0,0 +1,327 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2018 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From the PSync library https://github.com/named-data/PSync/blob/master/PSync/detail/iblt.cpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ *
+ * This file incorporates work covered by the following copyright and
+ * permission notice:
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Gavin Andresen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+// Only compile if ndn-cpp-config.h defines NDN_CPP_HAVE_LIBZ 1.
+#include <ndn-cpp/ndn-cpp-config.h>
+#if NDN_CPP_HAVE_LIBZ
+
+#include <stdexcept>
+#include <zlib.h>
+#include <ndn-cpp/lite/util/crypto-lite.hpp>
+#include "invertible-bloom-lookup-table.hpp"
+
+using namespace std;
+
+namespace ndn {
+
+InvertibleBloomLookupTable::InvertibleBloomLookupTable(size_t expectedNEntries)
+{
+  // 1.5 times the expected number of entries gives a very low probability of a
+  // decoding failure.
+  size_t nEntries = expectedNEntries + expectedNEntries / 2;
+  // Make nEntries exactly divisible by N_HASH.
+  size_t remainder = nEntries % N_HASH;
+  if (remainder != 0)
+    nEntries += (N_HASH - remainder);
+
+  hashTable_.resize(nEntries);
+}
+
+void
+InvertibleBloomLookupTable::initialize(const Blob& encoding)
+{
+  vector<uint32_t> values = decode(encoding);
+
+  if (3 * hashTable_.size() != values.size())
+    throw runtime_error("The received Invertible Bloom Filter cannot be decoded");
+
+  for (size_t i = 0; i < hashTable_.size(); i++) {
+    HashTableEntry& entry = hashTable_.at(i);
+    if (values[i * 3] != 0) {
+      entry.count_ = values[i * 3];
+      entry.keySum_ = values[(i * 3) + 1];
+      entry.keyCheck_ = values[(i * 3) + 2];
+    }
+  }
+}
+
+bool
+InvertibleBloomLookupTable::listEntries
+  (set<uint32_t>& positive, set<uint32_t>& negative) const
+{
+  // Make a deep copy.
+  InvertibleBloomLookupTable peeled(*this);
+
+  size_t nErased = 0;
+  do {
+    nErased = 0;
+    for (vector<HashTableEntry>::const_iterator entry = peeled.hashTable_.begin();
+         entry != peeled.hashTable_.end(); ++entry) {
+      if (entry->isPure()) {
+        if (entry->count_ == 1)
+          positive.insert(entry->keySum_);
+        else
+          negative.insert(entry->keySum_);
+
+        peeled.update(-entry->count_, entry->keySum_);
+        ++nErased;
+      }
+    }
+  } while (nErased > 0);
+
+  // If any buckets for one of the hash functions is not empty, then we didn't
+  // peel them all.
+  for (vector<HashTableEntry>::const_iterator entry = peeled.hashTable_.begin();
+       entry != peeled.hashTable_.end(); ++entry) {
+    if (!entry->isEmpty())
+      return false;
+  }
+
+  return true;
+}
+
+ptr_lib::shared_ptr<InvertibleBloomLookupTable>
+InvertibleBloomLookupTable::difference
+  (const InvertibleBloomLookupTable& other) const
+{
+  if (hashTable_.size() != other.hashTable_.size())
+    throw runtime_error("IBLT difference: Both tables must be the same size");
+
+  ptr_lib::shared_ptr<InvertibleBloomLookupTable> result =
+    ptr_lib::make_shared<InvertibleBloomLookupTable>(*this);
+  for (size_t i = 0; i < hashTable_.size(); i++) {
+    HashTableEntry& e1 = result->hashTable_.at(i);
+    const HashTableEntry& e2 = other.hashTable_.at(i);
+    e1.count_ -= e2.count_;
+    e1.keySum_ ^= e2.keySum_;
+    e1.keyCheck_ ^= e2.keyCheck_;
+  }
+
+  return result;
+}
+
+Blob
+InvertibleBloomLookupTable::encode() const
+{
+  size_t nEntries = hashTable_.size();
+  size_t unitSize = (32 * 3) / 8; // hard coding
+  size_t tableSize = unitSize * nEntries;
+
+  vector<uint8_t> table(tableSize);
+
+  for (size_t i = 0; i < nEntries; i++) {
+    // table[i*12],   table[i*12+1], table[i*12+2], table[i*12+3] --> hashTable[i].count_
+
+    table[(i * unitSize)]   = 0xFF & hashTable_[i].count_;
+    table[(i * unitSize) + 1] = 0xFF & (hashTable_[i].count_ >> 8);
+    table[(i * unitSize) + 2] = 0xFF & (hashTable_[i].count_ >> 16);
+    table[(i * unitSize) + 3] = 0xFF & (hashTable_[i].count_ >> 24);
+
+    // table[i*12+4], table[i*12+5], table[i*12+6], table[i*12+7] --> hashTable[i].keySum_
+
+    table[(i * unitSize) + 4] = 0xFF & hashTable_[i].keySum_;
+    table[(i * unitSize) + 5] = 0xFF & (hashTable_[i].keySum_ >> 8);
+    table[(i * unitSize) + 6] = 0xFF & (hashTable_[i].keySum_ >> 16);
+    table[(i * unitSize) + 7] = 0xFF & (hashTable_[i].keySum_ >> 24);
+
+    // table[i*12+8], table[i*12+9], table[i*12+10], table[i*12+11] --> hashTable[i].keyCheck_
+
+    table[(i * unitSize) + 8] = 0xFF & hashTable_[i].keyCheck_;
+    table[(i * unitSize) + 9] = 0xFF & (hashTable_[i].keyCheck_ >> 8);
+    table[(i * unitSize) + 10] = 0xFF & (hashTable_[i].keyCheck_ >> 16);
+    table[(i * unitSize) + 11] = 0xFF & (hashTable_[i].keyCheck_ >> 24);
+  }
+
+  return zlibCompress(table.data(), table.size());
+}
+
+bool
+InvertibleBloomLookupTable::equals(const InvertibleBloomLookupTable& other) const
+{
+  const vector<HashTableEntry>& iblt1HashTable = hashTable_;
+  const vector<HashTableEntry>& iblt2HashTable = other.hashTable_;
+  if (iblt1HashTable.size() != iblt2HashTable.size())
+    return false;
+
+  for (size_t i = 0; i < iblt1HashTable.size(); i++) {
+    if (iblt1HashTable[i].count_ != iblt2HashTable[i].count_ ||
+        iblt1HashTable[i].keySum_ != iblt2HashTable[i].keySum_ ||
+        iblt1HashTable[i].keyCheck_ != iblt2HashTable[i].keyCheck_)
+      return false;
+  }
+
+  return true;
+}
+
+void
+InvertibleBloomLookupTable::update(int plusOrMinus, uint32_t key)
+{
+  size_t bucketsPerHash = hashTable_.size() / N_HASH;
+
+  for (size_t i = 0; i < N_HASH; i++) {
+    size_t startEntry = i * bucketsPerHash;
+    uint32_t h = CryptoLite::murmurHash3(i, key);
+    HashTableEntry& entry = hashTable_.at(startEntry + (h % bucketsPerHash));
+    entry.count_ += plusOrMinus;
+    entry.keySum_ ^= key;
+    entry.keyCheck_ ^= CryptoLite::murmurHash3(N_HASHCHECK, key);
+  }
+}
+
+vector<uint32_t>
+InvertibleBloomLookupTable::decode(const Blob& encoding)
+{
+  Blob ibltValues = zlibDecompress(encoding.buf(), encoding.size());
+
+  size_t nEntries = ibltValues.size() / 4;
+  vector<uint32_t> values(nEntries, 0);
+
+  const uint8_t* ibltValuesBuf = ibltValues.buf();
+  for (size_t i = 0; i < 4 * nEntries; i += 4) {
+    uint32_t t = (ibltValuesBuf[i + 3] << 24) +
+                 (ibltValuesBuf[i + 2] << 16) +
+                 (ibltValuesBuf[i + 1] << 8)  +
+                 ibltValuesBuf[i];
+    values[i / 4] = t;
+  }
+
+  return values;
+}
+
+Blob
+InvertibleBloomLookupTable::zlibCompress(const uint8_t* data, size_t dataLength)
+{
+  // From https://www.zlib.net/zlib_how.html
+
+  // Use a vector in a shared_ptr so we can make it a Blob without copying.
+  ptr_lib::shared_ptr<vector<uint8_t>> result(new vector<uint8_t>());
+  uint8_t outBuffer[16384];
+
+  z_stream stream;
+  stream.zalloc = Z_NULL;
+  stream.zfree = Z_NULL;
+  stream.opaque = Z_NULL;
+  deflateInit(&stream, Z_BEST_COMPRESSION);
+
+  stream.next_in = const_cast<uint8_t*>(data);
+  stream.avail_in = dataLength;
+
+  int status;
+  do {
+    stream.avail_out = sizeof(outBuffer);
+    stream.next_out = outBuffer;
+    status = deflate(&stream, Z_FINISH);
+    if (!(status == Z_OK || status == Z_STREAM_END))
+      // We don't expect a compression error.
+      throw runtime_error("zlibCompress: Error during deflate");
+
+    size_t have = sizeof(outBuffer) - stream.avail_out;
+    result->insert(result->end(), outBuffer, outBuffer + have);
+  } while (stream.avail_out == 0);
+
+  deflateEnd(&stream);
+  if (status != Z_STREAM_END)
+    // We don't expect this error since stream.avail_out == 0.
+    throw runtime_error("zlibCompress: status is not Z_STREAM_END");
+
+  return Blob(result, false);
+}
+
+Blob
+InvertibleBloomLookupTable::zlibDecompress(const uint8_t* data, size_t dataLength)
+{
+  // From https://www.zlib.net/zlib_how.html
+
+  // Use a vector in a shared_ptr so we can make it a Blob without copying.
+  ptr_lib::shared_ptr<vector<uint8_t>> result(new vector<uint8_t>());
+  uint8_t outBuffer[16384];
+
+  z_stream stream;
+  stream.zalloc = Z_NULL;
+  stream.zfree = Z_NULL;
+  stream.opaque = Z_NULL;
+  stream.avail_in = 0;
+  stream.next_in = Z_NULL;
+  inflateInit(&stream);
+
+  stream.avail_in = dataLength;
+  stream.next_in = const_cast<uint8_t*>(data);
+
+  int status;
+  do {
+    stream.avail_out = sizeof(outBuffer);
+    stream.next_out = outBuffer;
+    status = inflate(&stream, Z_NO_FLUSH);
+    if (!(status == Z_OK || status == Z_STREAM_END))
+      throw runtime_error("zlibDecompress: Error during inflate");
+
+    size_t have = sizeof(outBuffer) - stream.avail_out;
+    result->insert(result->end(), outBuffer, outBuffer + have);
+  } while (stream.avail_out == 0);
+
+  inflateEnd(&stream);
+  if (status != Z_STREAM_END)
+    // We don't expect this error since stream.avail_out == 0.
+    throw runtime_error("zlibDecompress: status is not Z_STREAM_END");
+
+  return Blob(result, false);
+}
+
+bool
+InvertibleBloomLookupTable::HashTableEntry::isPure() const
+{
+  if (count_ == 1 || count_ == -1) {
+    uint32_t check = CryptoLite::murmurHash3(N_HASHCHECK, keySum_);
+    return keyCheck_ == check;
+  }
+
+  return false;
+}
+
+}
+
+#endif // NDN_CPP_HAVE_LIBZ
---
+++ src/sync/detail/invertible-bloom-lookup-table.hpp
@@ -0,0 +1,189 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2018 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From the PSync library https://github.com/named-data/PSync/blob/master/PSync/detail/iblt.hpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ *
+ * This file incorporates work covered by the following copyright and
+ * permission notice:
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Gavin Andresen
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef NDN_INVERTIBLE_BLOOM_LOOKUP_TABLE_HPP
+#define NDN_INVERTIBLE_BLOOM_LOOKUP_TABLE_HPP
+
+// Only compile if ndn-cpp-config.h defines NDN_CPP_HAVE_LIBZ 1.
+#include <ndn-cpp/ndn-cpp-config.h>
+#if NDN_CPP_HAVE_LIBZ
+
+#include <set>
+#include <ndn-cpp/util/blob.hpp>
+
+namespace ndn {
+
+/**
+ * InvertibleBloomLookupTable implements an Invertible Bloom Lookup Table (IBLT)
+ * (Invertible Bloom Filter). This is used by PSyncFullProducer.
+ */
+class InvertibleBloomLookupTable {
+public:
+  /**
+   * Create an InvertibleBloomLookupTable.
+   * @param expectedNEntries the expected number of entries in the IBLT.
+   */
+  InvertibleBloomLookupTable(size_t expectedNEntries);
+
+  /**
+   * Populate the hash table using the encoded vector representation of the IBLT.
+   * @param encoding the encoded representation of the IBLT.
+   * @throws runtime_error if the size of the decoded values is not compatible
+   * with this IBLT.
+   */
+  void
+  initialize(const Blob& encoding);
+
+  void
+  insert(uint32_t key) { update(INSERT, key); }
+
+  void
+  erase(uint32_t key) { update(ERASE, key); }
+
+  /**
+   * List all the entries in the IBLT.
+   * This is called on a difference of two IBLTs: ownIBLT - receivedIBLT.
+   * Entries listed in positive are in ownIBLT but not in receivedIBLT.
+   * Entries listed in negative are in receivedIBLT but not in ownIBLT.
+   * @param positive Add positive entries to this set. This does not first clear
+   * the set.
+   * @param negative Add negative entries to this set. This does not first clear
+   * the set.
+   * @return True if decoding is completed successfully.
+   */
+  bool
+  listEntries(std::set<uint32_t>& positive, std::set<uint32_t>& negative) const;
+
+  /**
+   * Get a new IBLT which is the difference of the other IBLT from this IBLT.
+   * @param other The other IBLT.
+   * @return A new IBLT of this - other.
+   */
+  ptr_lib::shared_ptr<InvertibleBloomLookupTable>
+  difference(const InvertibleBloomLookupTable& other) const;
+
+  /**
+   * Encode this IBLT to a Blob. This encodes this hash table from a uint32_t
+   * vector to a uint8_t vector. We create a uin8_t vector 12 times the size of
+   * the uint32_t vector. We put the first count in the first 4 cells, keySum in
+   * the next 4, and keyCheck in the next 4. We repeat for all the other cells
+   * of the hash table. Then we append this uint8_t vector to the name.
+   * @return The encoded Blob.
+   */
+  Blob
+  encode() const;
+
+  /**
+   * Check if this IBLT has the same number of entries as the other IBLT and
+   * that they are equal.
+   * @param other The other OBLT to check.
+   * @return true if this IBLT is equal to the other, otherwise false.
+   */
+  bool
+  equals(const InvertibleBloomLookupTable& other) const;
+
+  bool
+  operator== (const InvertibleBloomLookupTable &other) const { return equals(other); }
+
+  bool
+  operator!= (const InvertibleBloomLookupTable &other) const { return !equals(other); }
+
+  static const size_t N_HASH = 3;
+  static const size_t N_HASHCHECK = 11;
+
+private:
+  class HashTableEntry
+  {
+  public:
+    int32_t count_;
+    uint32_t keySum_;
+    uint32_t keyCheck_;
+
+    bool
+    isPure() const;
+
+    bool
+    isEmpty() const
+    {
+      return count_ == 0 && keySum_ == 0 && keyCheck_ == 0;
+    }
+  };
+
+  /**
+   * Update the entries in hashTable_.
+   * @param plusOrMinus The amount to update the count.
+   * @param key The key for computing the entry.
+   */
+  void
+  update(int plusOrMinus, uint32_t key);
+
+  /**
+   * Decode the IBLT from the Blob. This converts the Blob into a uint8_t vector
+   * which is then decoded to a uint32_t vector.
+   * @param encoding The encoded IBLT.
+   * @return A uint32_t vector representing the hash table of the IBLT.
+   */
+  static std::vector<uint32_t>
+  decode(const Blob& encoding);
+
+  static Blob
+  zlibCompress(const uint8_t* data, size_t dataLength);
+
+  static Blob
+  zlibDecompress(const uint8_t* data, size_t dataLength);
+
+  std::vector<HashTableEntry> hashTable_;
+
+  static const int INSERT = 1;
+  static const int ERASE = -1;
+};
+
+}
+
+#endif // NDN_CPP_HAVE_LIBZ
+
+#endif
