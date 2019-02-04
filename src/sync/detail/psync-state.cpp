5b71fdafade5b579a6c3c0d27e0fb936b40f2068
Jeff Thompson <jefft0@remap.ucla.edu>
2019-02-04 09:56:51 -0800

    sync (internal): Added PSyncState.

---
+++ src/sync/detail/psync-state.cpp
@@ -0,0 +1,96 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2018 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From the PSync library https://github.com/named-data/PSync/blob/master/PSync/detail/state.hpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#include "../../encoding/tlv-encoder.hpp"
+#include "../../encoding/tlv-decoder.hpp"
+#include "../../c/encoding/tlv/tlv-name.h"
+#include "psync-state.hpp"
+
+using namespace std;
+
+namespace ndn {
+
+Blob
+PSyncState::wireEncode() const
+{
+  TlvEncoder encoder(256);
+
+  encoder.writeNestedTlv(Tlv_PSyncContent, encodeContent, this);
+
+  return encoder.finish();
+}
+
+void
+PSyncState::encodeContent(const void *context, TlvEncoder &encoder)
+{
+  // Encode directly as TLV. We don't support the WireFormat abstraction
+  // because this isn't meant to go directly on the wire.
+  const PSyncState& pSyncState =  *(const PSyncState *)context;
+
+  for (size_t i = 0; i < pSyncState.content_.size(); ++i) {
+    const Name& name = pSyncState.content_[i];
+    struct ndn_NameComponent nameComponents[100];
+    NameLite nameLite
+      (nameComponents, sizeof(nameComponents) / sizeof(nameComponents[0]));
+    name.get(nameLite);
+
+    ndn_Error error;
+    size_t dummyBeginOffset, dummyEndOffset;
+    if ((error = ndn_encodeTlvName
+         (&nameLite, &dummyBeginOffset, &dummyEndOffset, &encoder)))
+      throw runtime_error(ndn_getErrorString(error));
+
+  }
+}
+
+void
+PSyncState::wireDecode(const uint8_t *input, size_t inputLength)
+{
+  clear();
+
+  // Decode directly as TLV. We don't support the WireFormat abstraction
+  // because this isn't meant to go directly on the wire.
+  TlvDecoder decoder(input, inputLength);
+  size_t endOffset = decoder.readNestedTlvsStart(Tlv_PSyncContent);
+
+  // Decode a sequence of Name.
+  while (decoder.offset < inputLength) {
+    struct ndn_NameComponent nameComponents[100];
+    NameLite nameLite
+      (nameComponents, sizeof(nameComponents) / sizeof(nameComponents[0]));
+
+    size_t encodingLength;
+    ndn_Error error;
+    size_t dummyBeginOffset, dummyEndOffset;
+    if ((error = ndn_decodeTlvName
+         (&nameLite, &dummyBeginOffset, &dummyEndOffset, &decoder)))
+      throw runtime_error(ndn_getErrorString(error));
+
+    Name name;
+    name.set(nameLite);
+    content_.push_back(name);
+  }
+
+  decoder.finishNestedTlvs(endOffset);
+}
+
+}
---
+++ src/sync/detail/psync-state.hpp
@@ -0,0 +1,122 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil -*- */
+/**
+ * Copyright (C) 2018 Regents of the University of California.
+ * @author: Jeff Thompson <jefft0@remap.ucla.edu>
+ * @author: From the PSync library https://github.com/named-data/PSync/blob/master/PSync/detail/state.hpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version, with the additional exemption that
+ * compiling, linking, and/or using OpenSSL is allowed.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * A copy of the GNU Lesser General Public License is in the file COPYING.
+ */
+
+#ifndef NDN_PSYNC_STATE_HPP
+#define NDN_PSYNC_STATE_HPP
+
+#include <ndn-cpp/name.hpp>
+
+namespace ndn {
+
+class TlvEncoder;
+
+class PSyncState {
+public:
+  /**
+   * Create a PSyncState with empty content.
+   */
+  PSyncState() {}
+
+  /**
+   * Create a PSyncState by decoding the input as an NDN-TLV PSyncContent.
+   * @param input A pointer to the input buffer to decode.
+   * @param inputLength The number of bytes in input.
+   */
+  PSyncState(const uint8_t *input, size_t inputLength)
+  {
+    wireDecode(input, inputLength);
+  }
+
+  /**
+   * Create a PSyncState by decoding the input as an NDN-TLV PSyncContent.
+   * @param input The input buffer to decode.
+   */
+  PSyncState(const Blob& input)
+  {
+    wireDecode(input);
+  }
+
+  /**
+   * Append the prefix to the content.
+   * @param prefix The prefix Name.
+   */
+  void
+  addContent(const Name& prefix) { content_.push_back(prefix); }
+
+  /**
+   * Get the sequence of prefix Names in the content.
+   * @return The vector of prefix Names.
+   */
+  const std::vector<Name>&
+  getContent() const { return content_; }
+
+  /**
+   * Remove the content.
+   */
+  void
+  clear() { content_.clear(); }
+
+  /**
+   * Encode this as an NDN-TLV PSyncContent.
+   * @return The encoding as a Blob.
+   */
+  Blob
+  wireEncode() const;
+
+  /**
+   * Decode the input as an NDN-TLV PSyncContent and update this object.
+   * @param input A pointer to the input buffer to decode.
+   * @param inputLength The number of bytes in input.
+   */
+  void
+  wireDecode(const uint8_t *input, size_t inputLength);
+
+  /**
+   * Decode the input as an NDN-TLV PSyncContent and update this object.
+   * @param input The input buffer to decode.
+   */
+  void
+  wireDecode(const Blob& input)
+  {
+    wireDecode(input.buf(), input.size());
+  }
+
+  enum {
+    Tlv_PSyncContent = 128
+  };
+
+private:
+  /**
+   * This is called by writeNestedTlv to encode the sequence of Names in
+   * the content.
+   * @param context A pointer to the PSyncState.
+   * @param encoder The TlvEncoder.
+   */
+  static void
+  encodeContent(const void *context, TlvEncoder &encoder);
+
+  std::vector<Name> content_;
+};
+
+}
+
+#endif
