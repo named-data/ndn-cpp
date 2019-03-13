03d0c42a8f73901dfa3b75d89331c590d6fd3b0b
Jeff Thompson <jefft0@remap.ucla.edu>
2019-03-13 11:34:30 -0700

    sync (internal): Make FullPSync2017WithUsers use FullPSync2017 as a utility class.

--- include/ndn-cpp/sync/full-psync2017-with-users.hpp
+++ include/ndn-cpp/sync/full-psync2017-with-users.hpp
@@ -23,15 +23,10 @@
 #ifndef NDN_FULL_PSYNC2017_WITH_USERS_HPP
 #define NDN_FULL_PSYNC2017_WITH_USERS_HPP
 
-#include "../face.hpp"
-#include "../util/segment-fetcher.hpp"
-#include "../security/key-chain.hpp"
-#include "psync-missing-data-info.hpp"
-#include "psync-producer-base.hpp"
+#include "full-psync2017.hpp"
 
 namespace ndn {
 
-class PSyncSegmentPublisher;
 class PSyncUserPrefixes;
 
 /**
@@ -49,10 +44,10 @@ class PSyncUserPrefixes;
  * in the name to avoid confusion.)
  */
 class FullPSync2017WithUsers {
+public:
   typedef func_lib::function<void
     (const ptr_lib::shared_ptr<std::vector<ptr_lib::shared_ptr<PSyncMissingDataInfo>>>& updates)> OnUpdate;
 
-public:
   /**
    * Create a FullPSync2017WithUsers.
    * @param expectedNEntries The expected number of entries in the IBLT.
@@ -69,24 +64,24 @@ public:
    * @param keyChain The KeyChain for signing Data packets.
    * @param syncInterestLifetime (optional) The Interest lifetime for the sync
    * Interests, in milliseconds. If omitted, use
-   * DEFAULT_SYNC_INTEREST_LIFETIME.
+   * FullPSync2017::DEFAULT_SYNC_INTEREST_LIFETIME.
    * @param syncReplyFreshnessPeriod (optional) The freshness period of the sync
    * Data packet, in milliseconds. If omitted, use
-   * DEFAULT_SYNC_REPLY_FRESHNESS_PERIOD.
+   * FullPSync2017::DEFAULT_SYNC_REPLY_FRESHNESS_PERIOD.
    * @param signingInfo (optional) The SigningInfo for signing Data packets,
    * which is copied. If omitted, use the default SigningInfo().
    */
   FullPSync2017WithUsers
     (size_t expectedNEntries, Face& face, const Name& syncPrefix,
      const Name& userPrefix, const OnUpdate& onUpdate, KeyChain& keyChain,
-     Milliseconds syncInterestLifetime = DEFAULT_SYNC_INTEREST_LIFETIME,
-     Milliseconds syncReplyFreshnessPeriod = DEFAULT_SYNC_REPLY_FRESHNESS_PERIOD,
+     Milliseconds syncInterestLifetime = FullPSync2017::DEFAULT_SYNC_INTEREST_LIFETIME,
+     Milliseconds syncReplyFreshnessPeriod = FullPSync2017::DEFAULT_SYNC_REPLY_FRESHNESS_PERIOD,
      const SigningInfo& signingInfo = SigningInfo())
-  : impl_(new Impl
-          (expectedNEntries, face, syncPrefix, userPrefix, onUpdate, keyChain,
-           syncInterestLifetime, syncReplyFreshnessPeriod, signingInfo))
+  : impl_(new Impl(onUpdate))
   {
-    impl_->initialize();
+    impl_->initialize
+      (expectedNEntries, face, syncPrefix, userPrefix, keyChain,
+       syncInterestLifetime, syncReplyFreshnessPeriod, signingInfo);
   }
 
   /**
@@ -138,34 +133,31 @@ public:
     impl_->publishName(prefix, sequenceNo);
   }
 
-  static const int DEFAULT_SYNC_REPLY_FRESHNESS_PERIOD = 1000;
-  static const int DEFAULT_SYNC_INTEREST_LIFETIME = 1000;
-
 private:
   /**
    * FullPSync2017WithUsers::Impl does the work of FullPSync2017WithUsers. It is a
    * separate class so that FullPSync2017WithUsers can create an instance in a
    * shared_ptr to use in callbacks.
    */
-  class Impl : public PSyncProducerBase {
+  class Impl : public ptr_lib::enable_shared_from_this<Impl> {
   public:
     /**
      * Create a new Impl, which should belong to a shared_ptr. Then you must
      * call initialize(). See the FullPSync2017WithUsers constructor for parameter
      * documentation.
      */
-    Impl
-      (size_t expectedNEntries, Face& face, const Name& syncPrefix,
-       const Name& userPrefix, const OnUpdate& onUpdate, KeyChain& keyChain,
-       Milliseconds syncInterestLifetime, Milliseconds syncReplyFreshnessPeriod,
-       const SigningInfo& signingInfo);
+    Impl(const OnUpdate& onUpdate);
 
     /**
      * Complete the work of the constructor. This is needed because we can't
      * call shared_from_this() in the constructor.
      */
     void
-    initialize();
+    initialize
+      (size_t expectedNEntries, Face& face, const Name& syncPrefix,
+       const Name& userPrefix, KeyChain& keyChain, 
+       Milliseconds syncInterestLifetime, Milliseconds syncReplyFreshnessPeriod,
+       const SigningInfo& signingInfo);
 
     int
     getSequenceNo(const Name& prefix) const;
@@ -176,84 +168,10 @@ private:
     void
     removeUserNode(const Name& prefix);
 
-    /**
-     * Publish the Name to inform the others.
-     * @param name The Name to publish.
-     */
-    void
-    publishName(const Name& name);
-
     void
     publishName(const Name& prefix, int sequenceNo);
 
   private:
-    class PendingEntryInfoFull {
-    public:
-      PendingEntryInfoFull
-        (const ptr_lib::shared_ptr<InvertibleBloomLookupTable> iblt)
-      : iblt_(iblt), isRemoved_(false)
-      {}
-
-      ptr_lib::shared_ptr<InvertibleBloomLookupTable> iblt_;
-      bool isRemoved_;
-    };
-
-    /**
-     * Send the sync interest for full synchronization. This forms the interest
-     * name: /<sync-prefix>/<own-IBLT>. This cancels any pending sync interest
-     * we sent earlier on the face.
-     */
-    void
-    sendSyncInterest();
-
-    /**
-     * Process a sync interest received from another party.
-     * This gets the difference between our IBLT and the IBLT in the other sync
-     * interest. If we cannot get the difference successfully, then send an
-     * application Nack. If we have some things in our IBLT that the other side
-     * does not have, then reply with the content. Or, if the number of
-     * different items is greater than threshold or equals zero, then send a
-     * Nack. Otherwise add the sync interest into the pendingEntries_ map with
-     * the interest name as the key and a PendingEntryInfoFull as the value.
-     * @param prefixName The prefix Name for the sync group which we registered.
-     * @param interest The the received Interest.
-     */
-    void
-    onSyncInterest
-      (const ptr_lib::shared_ptr<const Name>& prefixName,
-       const ptr_lib::shared_ptr<const Interest>& interest, Face& face,
-       uint64_t interestFilterId,
-       const ptr_lib::shared_ptr<const InterestFilter>& filter);
-
-    static void
-    onError(SegmentFetcher::ErrorCode errorCode, const std::string& message);
-
-    /**
-     * Send the sync Data. Check if the data will satisfy our own pending 
-     * Interest. If it does, then remove it and then renew the sync interest.
-     * Otherwise, just send the Data.
-     * @param name The basis to use for the Data name.
-     * @param content The content of the Data.
-     */
-    void
-    sendSyncData(const Name& name, Blob content);
-
-    /**
-     * Process the sync data after the content is assembled by the
-     * SegmentFetcher. Call deletePendingInterests to delete any pending sync
-     * Interest with the Interest name, which would have been satisfied by the
-     * forwarder once it got the data. For each prefix/sequenceNo in the data
-     * content,  check that we don't already have the prefix/sequenceNo, and
-     * call updateSeq(prefix, sequenceNo). Call onUpdate_ to notify the
-     * application about the updates. Call sendSyncInterest because the last one
-     * was satisfied by the incoming data.
-     * @param encodedContent The encoded sync data content that was assembled by
-     * the SegmentFetcher.
-     * @param interest The Interest for which we got the data.
-     */
-    void
-    onSyncData(Blob encodedContent, ptr_lib::shared_ptr<Interest>& interest);
-
     /**
      * This is called when new names are received to check if the name can be
      * added to the IBLT.
@@ -270,21 +188,6 @@ private:
      */
     void
     onNamesUpdate(const ptr_lib::shared_ptr<std::vector<Name>>& names);
-
-    /**
-     * Satisfy pending sync Interests. For a pending sync interests SI, if the
-     * IBLT of the sync Interest has any difference from our own IBLT, then
-     * send Data back. If we can't decode the difference from the stored IBLT,
-     * then delete it.
-     */
-    void
-    satisfyPendingInterests();
-
-    /**
-     * Delete pending sync Interests that match the given name.
-     */
-    void
-    deletePendingInterests(const Name& interestName);
 
     /**
      * Get the prefix from the name and check if hash(prefix + 1) is in the
@@ -310,28 +213,8 @@ private:
     bool
     updateSequenceNo(const Name& prefix, int sequenceNo);
 
-    /**
-     * Remove the entry from pendingEntries_ which has the name. However, if
-     * entry->isRemoved_ is true, do nothing. Therefore, if an entry is
-     * directly removed from pendingEntries_, it should set isRemoved_.
-     * @param name The key in the pendingEntries_ map for the entry to remove.
-     * @param entry A (possibly earlier and removed) entry from when it was
-     * inserted into the pendingEntries_ map.
-     * @param nonce This is only used for the log message.
-     */
-    void
-    delayedRemovePendingEntry
-      (const Name& name, const ptr_lib::shared_ptr<PendingEntryInfoFull>& entry,
-       const Blob& nonce);
-
-    Face& face_;
-    KeyChain& keyChain_;
-    ptr_lib::shared_ptr<PSyncSegmentPublisher> segmentPublisher_;
-    std::map<Name, ptr_lib::shared_ptr<PendingEntryInfoFull> > pendingEntries_;
-    Milliseconds syncInterestLifetime_;
     OnUpdate onUpdate_;
-    Name outstandingInterestName_;
-    uint64_t registeredPrefix_;
+    ptr_lib::shared_ptr<FullPSync2017> fullPSync_;
     ptr_lib::shared_ptr<PSyncUserPrefixes> prefixes_;
   };
 
--- src/sync/full-psync2017-with-users.cpp
+++ src/sync/full-psync2017-with-users.cpp
@@ -36,33 +36,26 @@ INIT_LOGGER("ndn.FullPSync2017WithUsers");
 
 namespace ndn {
 
-FullPSync2017WithUsers::Impl::Impl
-  (size_t expectedNEntries, Face& face, const Name& syncPrefix,
-   const Name& userPrefix, const OnUpdate& onUpdate, KeyChain& keyChain,
-   Milliseconds syncInterestLifetime, Milliseconds syncReplyFreshnessPeriod,
-   const SigningInfo& signingInfo)
-: PSyncProducerBase(expectedNEntries, syncPrefix, syncReplyFreshnessPeriod),
-  face_(face), keyChain_(keyChain), syncInterestLifetime_(syncInterestLifetime),
-  onUpdate_(onUpdate),
-  segmentPublisher_(new PSyncSegmentPublisher(face_, keyChain_)),
-  prefixes_(new PSyncUserPrefixes())
+FullPSync2017WithUsers::Impl::Impl(const OnUpdate& onUpdate)
+: onUpdate_(onUpdate), prefixes_(new PSyncUserPrefixes())
 {
-  if (userPrefix.size() > 0)
-    addUserNode(userPrefix);
 }
 
 void
-FullPSync2017WithUsers::Impl::initialize()
+FullPSync2017WithUsers::Impl::initialize
+  (size_t expectedNEntries, Face& face, const Name& syncPrefix,
+   const Name& userPrefix, KeyChain& keyChain, Milliseconds syncInterestLifetime,
+   Milliseconds syncReplyFreshnessPeriod, const SigningInfo& signingInfo)
 {
-  registeredPrefix_ = face_.registerPrefix
-    (syncPrefix_,
-     bind(&FullPSync2017WithUsers::Impl::onSyncInterest,
-     static_pointer_cast<FullPSync2017WithUsers::Impl>(shared_from_this()),
-     _1, _2, _3, _4, _5),
-     bind(&FullPSync2017WithUsers::Impl::onRegisterFailed, shared_from_this(), _1));
+  fullPSync_ = ptr_lib::make_shared<FullPSync2017>
+    (expectedNEntries, face, syncPrefix,
+     bind(&FullPSync2017WithUsers::Impl::onNamesUpdate, shared_from_this(), _1),
+     keyChain, syncInterestLifetime, syncReplyFreshnessPeriod, signingInfo,
+     bind(&FullPSync2017WithUsers::Impl::isNotFutureHash, shared_from_this(), _1, _2),
+     bind(&FullPSync2017WithUsers::Impl::canAddReceivedName, shared_from_this(), _1));
 
-  // TODO: Should we do this after the registerPrefix onSuccess callback?
-  sendSyncInterest();
+  if (userPrefix.size() > 0)
+    addUserNode(userPrefix);
 }
 
 int
@@ -83,16 +76,8 @@ FullPSync2017WithUsers::Impl::removeUserNode(const Name& prefix)
   if (prefixes_->isUserNode(prefix)) {
     int sequenceNo = prefixes_->prefixes_[prefix];
     prefixes_->removeUserNode(prefix);
-    removeFromIblt(Name(prefix).appendNumber(sequenceNo));
+    fullPSync_->removeName(Name(prefix).appendNumber(sequenceNo));
   }
-}
-
-void
-FullPSync2017WithUsers::Impl::publishName(const Name& name)
-{
-  _LOG_TRACE("Publish: " << name);
-  insertIntoIblt(name);
-  satisfyPendingInterests();
 }
 
 void
@@ -108,303 +93,7 @@ FullPSync2017WithUsers::Impl::publishName(const Name& prefix, int sequenceNo)
   _LOG_TRACE("Publish: " << prefix << "/" << newSequenceNo);
   if (updateSequenceNo(prefix, newSequenceNo))
     // Insert the new sequence number.
-    publishName(Name(prefix).appendNumber(newSequenceNo));
-}
-
-void
-FullPSync2017WithUsers::Impl::sendSyncInterest()
-{
-#if 0 // Debug: Implement stopping an ongoing fetch.
-  // If we send two sync interest one after the other
-  // since there is no new data in the network yet,
-  // when data is available it may satisfy both of them
-  if (fetcher_) {
-    fetcher_->stop();
-  }
-#endif
-
-  // Sync Interest format for full sync: /<sync-prefix>/<ourLatestIBF>
-  Name syncInterestName(syncPrefix_);
-
-  // Append our latest IBLT.
-  syncInterestName.append(iblt_->encode());
-
-  outstandingInterestName_ = syncInterestName;
-
-  // random1 is from 0.0 to 1.0.
-  float random1;
-  CryptoLite::generateRandomFloat(random1);
-  // Get a jitter of +/- syncInterestLifetime_ * 0.2 .
-  Milliseconds jitter = (random1 - 0.5) * (syncInterestLifetime_ * 0.2);
-
-  face_.callLater
-    (syncInterestLifetime_ / 2 + jitter, 
-     bind(&FullPSync2017WithUsers::Impl::sendSyncInterest,
-          static_pointer_cast<FullPSync2017WithUsers::Impl>(shared_from_this())));
-
-  ptr_lib::shared_ptr<Interest> syncInterest
-    (ptr_lib::make_shared<Interest>(syncInterestName));
-  syncInterest->setInterestLifetimeMilliseconds(syncInterestLifetime_);
-  syncInterest->refreshNonce();
-
-  SegmentFetcher::fetch
-    (face_, *syncInterest, 0,
-     bind(&FullPSync2017WithUsers::Impl::onSyncData,
-          static_pointer_cast<FullPSync2017WithUsers::Impl>(shared_from_this()), _1, syncInterest),
-     &FullPSync2017WithUsers::Impl::onError);
-
-  _LOG_DEBUG("sendFullSyncInterest, nonce: " << syncInterest->getNonce() <<
-             ", hash: " << syncInterestName.hash());
-}
-
-void
-FullPSync2017WithUsers::Impl::onError
-  (SegmentFetcher::ErrorCode errorCode, const std::string& message)
-{
-  _LOG_TRACE("Cannot fetch sync data, error: " << errorCode <<
-             " message: " << message);
-}
-
-void
-FullPSync2017WithUsers::Impl::onSyncInterest
-  (const ptr_lib::shared_ptr<const Name>& prefixName,
-   const ptr_lib::shared_ptr<const Interest>& interest, Face& face,
-   uint64_t interestFilterId,
-   const ptr_lib::shared_ptr<const InterestFilter>& filter)
-{
-  if (segmentPublisher_->replyFromStore(interest->getName()))
-    return;
-
-  Name nameWithoutSyncPrefix = interest->getName().getSubName(prefixName->size());
-  Name interestName;
-
-  if (nameWithoutSyncPrefix.size() == 1)
-    // Get /<prefix>/IBLT from /<prefix>/IBLT
-    interestName = interest->getName();
-  else if (nameWithoutSyncPrefix.size() == 3)
-    // Get /<prefix>/IBLT from /<prefix>/IBLT/<version>/<segment-no>
-    interestName = interest->getName().getPrefix(-2);
-  else
-    return;
-
-  Name::Component ibltName = interestName.get(-1);
-
-  _LOG_DEBUG("Full Sync Interest received, nonce: " << interest->getNonce() <<
-             ", hash:" << interestName.hash());
-
-  ptr_lib::shared_ptr<InvertibleBloomLookupTable> iblt
-    (new InvertibleBloomLookupTable(expectedNEntries_));
-  try {
-    iblt->initialize(ibltName.getValue());
-  } catch (const std::exception& ex) {
-    _LOG_ERROR(ex.what());
-    return;
-  }
-
-  ptr_lib::shared_ptr<InvertibleBloomLookupTable> difference =
-   iblt_->difference(*iblt);
-
-  set<uint32_t> positive;
-  set<uint32_t> negative;
-
-  if (!difference->listEntries(positive, negative)) {
-    _LOG_TRACE("Cannot decode differences, positive: " << positive.size() <<
-            " negative: " << negative.size() << " m_threshold: " <<
-            threshold_);
-
-    // Send all data if greater then the threshold, or if there are neither
-    // positive nor negative differences. Otherwise, continue below and send
-    // the positive as usual.
-    if (positive.size() + negative.size() >= threshold_ ||
-        (positive.size() == 0 && negative.size() == 0)) {
-      PSyncState state1;
-      for (map<Name, uint32_t>::iterator entry = nameToHash_.begin();
-           entry != nameToHash_.end(); ++entry)
-        state1.addContent(entry->first);
-
-      if (state1.getContent().size() > 0)
-        segmentPublisher_->publish
-          (interest->getName(), interest->getName(), state1.wireEncode(),
-           syncReplyFreshnessPeriod_);
-
-      return;
-    }
-  }
-
-  PSyncState state;
-  for (set<uint32_t>::iterator hash = positive.begin(); hash != positive.end();
-       ++hash) {
-    Name name = hashToName_[*hash];
-
-    if (nameToHash_.find(name) != nameToHash_.end()) {
-      if (isNotFutureHash(name, negative))
-        state.addContent(name);
-    }
-  }
-
-  if (state.getContent().size() > 0) {
-    _LOG_DEBUG("Sending sync content: " << state.toString());
-    sendSyncData(interestName, state.wireEncode());
-    return;
-  }
-
-  ptr_lib::shared_ptr<PendingEntryInfoFull> entry(new PendingEntryInfoFull(iblt));
-  pendingEntries_[interestName] = entry;
-  face_.callLater
-    (interest->getInterestLifetimeMilliseconds(),
-     bind(&FullPSync2017WithUsers::Impl::delayedRemovePendingEntry,
-          static_pointer_cast<FullPSync2017WithUsers::Impl>(shared_from_this()),
-          interest->getName(), entry, interest->getNonce()));
-}
-
-void
-FullPSync2017WithUsers::Impl::sendSyncData(const Name& name, Blob content)
-{
-  _LOG_DEBUG("Checking if the Data will satisfy our own pending interest");
-
-  Name nameWithIblt;
-  nameWithIblt.append(iblt_->encode());
-
-  // Append the hash of our IBLT so that the Data name should be different for
-  // each node.
-  Name dataName(Name(name).appendNumber(nameWithIblt.hash()));
-
-  // Check if our own Interest got satisfied.
-  if (outstandingInterestName_.equals(name)) {
-    _LOG_DEBUG("Satisfies our own pending Interest");
-    // remove outstanding interest
-#if 0 // Debug: Implement stopping an ongoing fetch.
-    if (fetcher_) {
-      _LOG_DEBUG("Removing our pending interest from the Face (stopping fetcher)");
-      fetcher_->stop();
-      outstandingInterestName_ = Name();
-    }
-#else
-    outstandingInterestName_ = Name();
-#endif
-
-    _LOG_DEBUG("Sending sync Data");
-
-    // Send Data after removing the pending sync interest on the Face.
-    segmentPublisher_->publish(name, dataName, content, syncReplyFreshnessPeriod_);
-
-    _LOG_TRACE("sendSyncData: Renewing sync interest");
-    sendSyncInterest();
-  }
-  else {
-    _LOG_DEBUG("Sending Sync Data for not our own Interest");
-    segmentPublisher_->publish(name, dataName, content, syncReplyFreshnessPeriod_);
-  }
-}
-
-void
-FullPSync2017WithUsers::Impl::satisfyPendingInterests()
-{
-  _LOG_DEBUG("Satisfying full sync Interest: " << pendingEntries_.size());
-
-  for (map<Name, ptr_lib::shared_ptr<PendingEntryInfoFull> >::iterator it =
-         pendingEntries_.begin();
-       it != pendingEntries_.end();) {
-    const InvertibleBloomLookupTable& entryIblt = *it->second->iblt_;
-    ptr_lib::shared_ptr<InvertibleBloomLookupTable> difference =
-            iblt_->difference(entryIblt);
-    set<uint32_t> positive;
-    set<uint32_t> negative;
-
-    if (!difference->listEntries(positive, negative)) {
-      _LOG_TRACE("Decode failed for pending interest");
-      if (positive.size() + negative.size() >= threshold_ ||
-          (positive.size() == 0 && negative.size() == 0)) {
-        _LOG_TRACE
-          ("positive + negative > threshold or no difference can be found. Erase pending interest.");
-        // Prevent delayedRemovePendingEntry from removing a new entry with the same Name.
-        it->second->isRemoved_ = true;
-        pendingEntries_.erase(it++);
-        continue;
-      }
-    }
-
-    PSyncState state;
-    for (set<uint32_t>::iterator hash = positive.begin(); hash != positive.end();
-         ++hash) {
-      Name name = hashToName_[*hash];
-
-      if (nameToHash_.find(name) != nameToHash_.end())
-        state.addContent(name);
-    }
-
-    if (state.getContent().size() > 0) {
-      _LOG_DEBUG("Satisfying sync content: " << state.toString());
-      sendSyncData(it->first, state.wireEncode());
-      // Prevent delayedRemovePendingEntry from removing a new entry with the same Name.
-      it->second->isRemoved_ = true;
-      pendingEntries_.erase(it++);
-    }
-    else
-      ++it;
-  }
-}
-
-void
-FullPSync2017WithUsers::Impl::deletePendingInterests(const Name& interestName)
-{
-  for (map<Name, ptr_lib::shared_ptr<PendingEntryInfoFull> >::iterator it =
-         pendingEntries_.begin();
-       it != pendingEntries_.end();) {
-    if (it->first.equals(interestName)) {
-      _LOG_TRACE("Delete pending interest: " << interestName);
-      // Prevent delayedRemovePendingEntry from removing a new entry with the same Name.
-      it->second->isRemoved_ = true;
-      pendingEntries_.erase(it++);
-    }
-    else
-      ++it;
-  }
-}
-
-void
-FullPSync2017WithUsers::Impl::onSyncData
-  (Blob encodedContent, ptr_lib::shared_ptr<Interest>& interest)
-{
-  deletePendingInterests(interest->getName());
-
-  PSyncState state(encodedContent);
-  ptr_lib::shared_ptr<vector<Name>> names
-    (ptr_lib::make_shared<vector<Name>>());
-
-  _LOG_DEBUG("Sync Data Received: " << state.toString());
-
-  const std::vector<Name>& content = state.getContent();
-  for (vector<Name>::const_iterator contentName = content.begin();
-       contentName != content.end(); ++contentName) {
-    _LOG_DEBUG("Checking whether to add " << *contentName);
-    if (canAddReceivedName(*contentName)) {
-      _LOG_DEBUG("Adding name " << *contentName);
-      names->push_back(*contentName);
-      insertIntoIblt(*contentName);
-    }
-    // We should not call satisfyPendingSyncInterests here because we just
-    // got data and deleted pending interests by calling deletePendingInterests.
-    // But we might have interests which don't match this interest that might
-    // not have been deleted from the pending sync interests.
-  }
-
-  // We just got the data, so send a new sync Interest.
-  if (names->size() > 0) {
-    try {
-      onNamesUpdate(names);
-    } catch (const std::exception& ex) {
-      _LOG_ERROR("Error in onUpdate: " << ex.what());
-    } catch (...) {
-      _LOG_ERROR("Error in onUpdate.");
-    }
-
-    _LOG_TRACE("onSyncData: Renewing sync interest");
-    sendSyncInterest();
-  } else {
-    _LOG_TRACE("No new update, interest nonce: " << interest->getNonce().toHex() <<
-            " , hash: " << interest->getName().hash());
-  }
+    fullPSync_->publishName(Name(prefix).appendNumber(newSequenceNo));
 }
 
 bool
@@ -420,7 +109,7 @@ FullPSync2017WithUsers::Impl::canAddReceivedName(const Name& name)
       if (oldSequenceNo != 0)
         // Remove the old sequence number form the IBLT before the caller adds
         // the new one.
-        removeFromIblt(Name(prefix).appendNumber(oldSequenceNo));
+        fullPSync_->removeName(Name(prefix).appendNumber(oldSequenceNo));
     }
 
     return true;
@@ -489,24 +178,9 @@ FullPSync2017WithUsers::Impl::updateSequenceNo(const Name& prefix, int sequenceN
   // don't need to delete it, because we don't insert a prefix with sequence
   // number zero in the IBLT.
   if (oldSequenceNo != 0)
-    removeFromIblt(Name(prefix).appendNumber(oldSequenceNo));
+    fullPSync_->removeName(Name(prefix).appendNumber(oldSequenceNo));
 
   return true;
-}
-
-void
-FullPSync2017WithUsers::Impl::delayedRemovePendingEntry
-  (const Name& name, const ptr_lib::shared_ptr<PendingEntryInfoFull>& entry,
-   const Blob& nonce)
-{
-  if (entry->isRemoved_)
-    // A previous operation already removed this entry, so don't try again to
-    // remove the entry with the Name in case it is a new entry.
-    return;
-
-  _LOG_TRACE("Remove Pending Interest " << nonce);
-  entry->isRemoved_ = true;
-  pendingEntries_.erase(name);
 }
 
 }
