a1c738ab62453c01c84ce72f6e033e990631f50d
jefft0 <>
2020-01-02 13:20:35 -0800

    ThreadsafeFace. Fix bug in registerPrefix to use a copy of registrationOptions when dispatching.

--- src/threadsafe-face.cpp
+++ src/threadsafe-face.cpp
@@ -135,7 +135,7 @@ ThreadsafeFace::registerPrefix
     (boost::bind
      (&Node::registerPrefix, node_, registeredPrefixId,
       ptr_lib::make_shared<const Name>(prefix), onInterest, onRegisterFailed,
-      onRegisterSuccess, boost::ref(registrationOptions), boost::ref(wireFormat), this));
+      onRegisterSuccess, registrationOptions, boost::ref(wireFormat), this));
 
   return registeredPrefixId;
 }
